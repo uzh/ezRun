7bcac92a2271976154fef0677879c63377169e15
pdschmid <peter.schmid@ieu.uzh.ch>
2015-10-12 17:11:06 +0200

    ezBuildRefDir works now

--- R/02references.R
+++ R/02references.R
@@ -1,11 +1,9 @@
-
-# Saccharomyces_cerevisiae/Ensembl/EF4/Annotation/Version-2013-03-18
-
-
-ezBuildRefDir = function(refBuild){
-  ## make the directory structure
-}
-
+###################################################################
+# Functional Genomics Center Zurich
+# This code is distributed under the terms of the GNU General
+# Public License Version 3, June 2007.
+# The terms are available here: http://www.gnu.org/licenses/gpl.html
+# www.fgcz.ch
 
 
 ##' @title The S4 class representing a collection of information about the reference build
@@ -21,7 +19,12 @@ ezBuildRefDir = function(refBuild){
 ##' @slot refChromSizesFile a character specifying the file path to the file containing the chromosome sizes.
 ##' @template roxygen-template
 ##' @examples
-##' EzRef(param=list(refBuild="foo"))
+##' refBuild = "Saccharomyces_cerevisiae/Ensembl/EF4/Annotation/Version-2013-03-18"
+##' gtf = "genes.gtf"
+##' fasta = "genome.fa"
+##' genomesRoot = "~/refExample"
+##' myRef = EzRef(param=ezParam(list(refBuild=refBuild)), genomesRoot=genomesRoot)
+##' ezBuildRefdir(myRef, gtf, fasta, genomesRoot)
 EzRef = setClass("EzRef",
                  slots = c(refBuild="character",
                            refBuildName="character",
@@ -31,10 +34,11 @@ EzRef = setClass("EzRef",
                            refAnnotationFile="character",
                            refFastaFile="character",
                            refChromDir="character",
-                           refChromSizesFile="character"))
+                           refChromSizesFile="character",
+                           refAnnotationVersion="character"))
 
-##' @describeIn EzRef Initializes the slots of EzRef. It will fill also try to specify some fields and if necessary get full file paths.
-setMethod("initialize", "EzRef", function(.Object, param=list()){
+##' @describeIn EzRef Initializes the slots of EzRef. It will also try to specify some fields and if necessary get full file paths.
+setMethod("initialize", "EzRef", function(.Object, param=list(), genomesRoot=GENOMES_ROOT){
   #   if (!ezIsSpecified(param$refBuild)){
   #     return(.Object)
   #   }
@@ -48,8 +52,13 @@ setMethod("initialize", "EzRef", function(.Object, param=list()){
   if (ezIsSpecified(param$refBuildDir)){
     .Object@refBuildDir = param$refBuildDir
   } else {
-    .Object@refBuildDir = file.path(GENOMES_ROOT, paste(refFields[1:3], collapse="/"))
-  }  
+    .Object@refBuildDir = file.path(genomesRoot, paste(refFields[1:3], collapse="/"))
+  }
+  if (length(refFields) == 5 && grepl("^Version", refFields[5])){
+    .Object@refAnnotationVersion = refFields[5]
+  } else {
+    NULL
+  }
   if (ezIsSpecified(param$refIndex)){
     .Object@refIndex = param$refIndex
   } else {
@@ -58,7 +67,7 @@ setMethod("initialize", "EzRef", function(.Object, param=list()){
   if (ezIsAbsolutePath(param$refFeatureFile)){
     .Object@refFeatureFile = param$refFeatureFile
   } else {
-    .Object@refFeatureFile =  file.path(.Object@refBuildDir, "Annotation/Genes", param$refFeatureFile)
+    .Object@refFeatureFile =  file.path(.Object@refBuildDir, "Annotation", .Object@refAnnotationVersion, "Genes", param$refFeatureFile)
   }
   if (ezIsAbsolutePath(param$refAnnotationFile)){
     .Object@refAnnotationFile = param$refAnnotationFile
@@ -86,4 +95,24 @@ setMethod("initialize", "EzRef", function(.Object, param=list()){
 ##' @describeIn EzRef Allows selection with square brackets [ ].
 setMethod("[", "EzRef", function(x, i){
   slot(x, i)
+})
+
+setGeneric("ezBuildRefdir", function(.Object, refBuild, gtf, fasta, genomesRoot = "."){
+  standardGeneric("ezBuildRefdir")
+})
+
+##' @describeIn Builds the reference directory and copies the annotation and fast file into the right folders.
+setMethod("ezBuildRefdir", "EzRef", function(.Object, refBuild, gtf, fasta, genomesRoot = "."){
+  cd = getwd()
+  setwdNew(genomesRoot)
+  gtfPath = dirname(.Object@refFeatureFile)
+  fastaPath = dirname(.Object@refFastaFile)
+  dir.create(gtfPath, recursive=T)
+  dir.create(fastaPath, recursive=T)
+  dir.create(.Object@refChromDir)
+  #gtf = list.files(pattern=".gtf$")
+  #fasta = list.files(pattern=".fa$")
+  file.copy(gtf, gtfPath)
+  file.copy(fasta, fastaPath)
+  setwd(cd)
 })
