81551b032e42b4f35eb666dd9b699a0829ee7f8b
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2015-10-13 16:40:01 +0200

    update test script

--- script/runningLongTests.R
+++ script/runningLongTests.R
@@ -1,6 +1,7 @@
 
 ## settings for running the tests as Hubert
 .libPaths("/srv/GT/analysis/course_sushi/lib")
+EZ_GLOBAL_VARIABLES <<- '/usr/local/ngseq/opt/EZ_GLOBAL_VARIABLES.txt'
 require(ezRun)
 EZ_PARAM_DEFAULTS["adminMail", "DefaultValue"] = "Hubert.Rehrauer@fgcz.ethz.ch"
 Sys.setenv(RUN_LONG_TEST=TRUE)
@@ -9,6 +10,7 @@ devtools::test()
 
 ## settings for running the tests as Peter
 .libPaths("/home/pdschmid/R-libs")
+EZ_GLOBAL_VARIABLES <<- '/usr/local/ngseq/opt/EZ_GLOBAL_VARIABLES.txt'
 setwd("/home/pdschmid/ezRun")
 require(ezRun)
 EZ_PARAM_DEFAULTS["adminMail", "DefaultValue"] = "peter.schmid@ieu.uzh.ch"
