1060425e68c2dc2005a1fa29695c0d35eb033887
pdschmid <peter.schmid@ieu.uzh.ch>
2015-10-29 10:43:40 +0100

    gg plot stuff

---
+++ script/planForGGplots.R
@@ -0,0 +1,139 @@
+
+
+
+EzPlotter2 =
+  setRefClass("EzPlotter2",
+              fields = c("name", "ggPlot", "helpText", "mouseOverText"),
+              methods = list(
+                initialize = function(ggPlot, name=NULL)
+                {
+                  if (ezIsSpecified(name)){
+                    name <<- name
+                  } else {
+                    name <<- "EzPlotter2"
+                  }
+                  ggPlot <<- ggPlot
+                  helpText <<- "Default helpText"
+                  mouseOverText <<- "Default mouseOverText"
+                },
+                plotPng = function(file=NULL, width=480, height=480)
+                {
+                  "Creates a .png file of a plot."
+                  if (ezIsSpecified(file)) {
+                    filename = file
+                  } else {
+                    filename = paste0(name, ".png")
+                  }
+                  png(filename = filename, width, height)
+                  plot(ggPlot)
+                  dev.off()
+                  return(filename)
+                },
+                plotPdf = function(file=NULL, width=480, height=480)
+                {
+                  "Creates a .pdf file of a plot."
+                  if (ezIsSpecified(file)) {
+                    filename = file
+                  } else {
+                    filename = paste0(name, ".pdf")
+                  }
+                  width = round(width/72, digits=2)
+                  height = round(height/72, digits=2)
+                  pdf(file = filename, width, height)
+                  plot(ggPlot)
+                  dev.off()
+                  return(filename)
+                }
+              )
+  )
+
+ezImageFileLink2 = function(ggPlot, file=NULL, mouseOverText=ezPlotter$mouseOverText,
+                            addPdfLink=TRUE, width=480, height=480){
+  ezPlotter = EzPlotter2$new(ggPlot)
+  pngName = ezPlotter$plotPng(file=file, width=width, height=height)
+  if (addPdfLink) {
+    pdfName = ezPlotter$plotPdf(file=sub(".png$", ".pdf", file), width=width, height=height)
+    imgFilePot = pot(paste('<img src="', pngName, '" title="', mouseOverText, '"/>'),
+                     hyperlink = pdfName)
+  } else {
+    imgFilePot = pot(paste('<img src="', pngName, '" title="', mouseOverText, '"/>'))
+  }
+  return(as.html(imgFilePot))
+}
+
+ezGGplot = function(...){
+  ggPlot = ggplot(...) + theme_bw(base_size=14) + theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank())
+  return(ggPlot)
+}
+
+cd2 = getwd()
+setwdNew("./scratch")
+theDoc = bsdoc(title = 'My document')
+theDoc = addTitle(theDoc, "A title")
+myPlot = qplot(iris$Sepal.Length, iris$Sepal.Width)
+myLink = ezImageFileLink2(myPlot, file="test.png")
+theDoc = addParagraph(theDoc, myLink)
+writeDoc(theDoc, "my.html")
+setwd(cd2)
+
+
+ggVolcano = function(log2Ratio, pValue, xlim=NULL, ylim=NULL,
+                     isPresent=NULL, types=NULL, pch=16,
+                     colors=rainbow(ncol(types)),
+                     legendPos="bottomright", yType="p-value"){
+  
+  yValues = -log10(pValue)
+  if (is.null(xlim)){
+    lrMaxAbs = max(abs(log2Ratio), na.rm=TRUE)
+    xm = min(lrMaxAbs, 5)
+    xlim = c(-xm, xm)
+    log2Ratio = shrinkToRange(log2Ratio, theRange = xlim)
+  }
+  if (is.null(ylim)){
+    ym = min(max(yValues, na.rm=TRUE), 10)
+    ylim = c(0, ym)
+    yValues = shrinkToRange(yValues, theRange=ylim)
+  }
+  
+  ggPlot = ezGGplot(data=df, aes(log2Ratio, yValues))
+  ggPlot = ggPlot + theme(aspect.ratio=1) +
+    xlab("log2 ratio") + ylab(paste0("-log10(", yType, ")")) +
+    xlim(xlim) + ylim(ylim)
+  ggPlot = ggPlot + geom_point(color="grey", shape=pch)
+  
+  if (!is.null(isPresent)){
+    ggPlot = ggPlot + geom_point(color="black", shape=pch, aes(log2Ratio[isPresent], yValues[isPresent]))
+  }
+  if (!is.null(types)){
+    for (j in 1:ncol(types)){
+      loopHelp = paste0("geom_point(shape=", pch, ",color='", colors[j], "',aes(", log2Ratio[types[,j]], ",", yValues[types[,j]], "))")
+      # loopHelp = paste0("geom_point(shape=", pch, ",aes(", log2Ratio[types[,j]], ",", yValues[types[,j]], ",color='", colors[j], "'))") ## for the legend
+      ggPlot = ggPlot + eval(parse(text=loopHelp))
+    }
+    ggPlot = ggPlot + scale_color_manual(values=colors)
+    if (!is.null(legendPos)){
+      # legend(legendPos, colnames(types), col=colors, cex=1.2, pch=20, bty="o", pt.bg="white")
+      #       ggPlot = ggPlot + theme(legend.position=legendPos) + geom_point(aes())
+      #       ggPlot
+    }
+  }
+  return(ggPlot)
+}
+
+
+# ggtitle("title")
+
+# log2Ratio = 1:100
+# yValues = -log10(rep(10^(-4:5), each=10))
+# xlim=c(0,100)
+# ylim=c(-5,4)
+# pch=16
+# isPresent=1:50
+# types = data.frame(matrix(rep(1:10, each=10), 10))
+# colors=rainbow(ncol(types))
+# legendPos="bottomright"
+# cex.main=1.0
+# cex=0.8
+# yType="p-value"
+
+
--- script/planForPlots.R
+++ script/planForPlots.R
@@ -1,191 +1,63 @@
 
 require(ReporteRs)
 
-# cd = getwd()
-# setwdNew("./scratch")
-# 
-# theDoc = bsdoc(title = 'My document')
-# theDoc = addTitle(theDoc, "A title")
-# irislink = ezImageFileLink(EzPlotterIris$new(name="Iris"),
-#                     main="Iris sepal shape", xlab="Iris sepal length", ylab="Iris sepal width",
-#                     width=600,height=600)
-# theDoc = addParagraph(theDoc, irislink)
-# types = data.frame(matrix(rep(1:10, each=10), 10))
-# volcanolink = ezImageFileLink(EzPlotterVolcano$new(name="Volcano2", log2Ratio=1:100, pValue=rep(10^(-4:5), each=10)),
-#                     xlim=c(0,100), ylim=c(-5,4), pch=16, isPresent=1:50, types=types,
-#                     colors=rainbow(ncol(types)), legendPos="bottomleft",
-#                     main="Volcano2")
-# theDoc = addParagraph(theDoc, volcanolink)
-# scascalink = ezImageFileLink(EzPlotterXYScatterScatter$new(name="ScatterScatter", xVec=1:10, yVec=1:10),
-#                     main="ScatterScatter")
-# theDoc = addParagraph(theDoc, scascalink)
-# x = ezMatrix("", rows=1, cols=c("iris", "xy", "iris2"))
-# x[1, "iris"] = ezImageFileLink(EzPlotterIris$new(name="Iris2"))
-# x[1, "xy"] = ezImageFileLink(EzPlotterXYScatterScatter$new(name="ScatterScatterScatter", xVec=1:10, yVec=1:10))
-# x[1, "iris2"] = ezImageFileLink(EzPlotterIris$new(name="Iris3"))
-# theDoc = addFlexTable(theDoc, ezFlexTable(x))
-# vier = ezImageFileLink(EzPlotterIris$new(name="Iris4"))
-# fuenf = ezImageFileLink(EzPlotterIris$new(name="Iris5"))
-# theDoc = addFlexTable(theDoc, ezFlexTable(cbind(vier,fuenf)))
-# smoscafilink = ezImageFileLink(EzPlotterSmoothScatter$new(name="SmoothScatter", y=data.frame(a=1:10,b=21:30)), file="SmoothScatterplot.png", cex=2)
-# theDoc = addParagraph(theDoc, smoscafilink)
-# smoscafilink2 = ezImageFileLink(EzPlotterSmoothScatter$new(name="SmoothScatterMultiple", y=data.frame(a=1:10,b=21:30,c=90:81), x=51:60))
-# theDoc = addParagraph(theDoc, smoscafilink2)
-# scatterlink = ezImageFileLink(EzPlotterScatter$new(name="Scatter", y=data.frame(a=1:10,b=21:30)))
-# theDoc = addParagraph(theDoc, scatterlink)
-# scatterlink2 = ezImageFileLink(EzPlotterScatter$new(name="ScatterMultiple", y=data.frame(a=1:10,b=21:30,c=90:81), x=51:60))
-# theDoc = addParagraph(theDoc, scatterlink2)
-# allscatterlink = ezImageFileLink(EzPlotterAllPairScatter$new(x=matrix(1:10,5)))
-# theDoc = addParagraph(theDoc, allscatterlink)
-# allscatterlink2 = ezImageFileLink(EzPlotterAllPairScatter$new(name="allshallscatter", x=matrix(1:25,5)))
-# theDoc = addParagraph(theDoc, allscatterlink2)
-# 
-# theList = list()
-# for (i in 1:4){
-#   theList[[i]] = ezImageFileLink(EzPlotterIris$new(name=paste("Iris",i+10,sep="")))
-# }
-# theDoc = addFlexTable(theDoc, ezFlexTable(rbind(theList[1:4])))
-# 
-# testLink = "testLink.txt"
-# theDoc = addParagraph(theDoc, pot(paste("<a href='", testLink, "' type='application/text'>", testLink, "</a>")))
-# 
-# # x = ezMatrix("", rows=1, cols=sampleNames)
-# # for (nm in sampleNames){
-# #   x[1, nm] = ezImageFileLink(EzPlotterIris$new(name="Iris2", data[sm]))
-# # }
-# # theDoc = addFlexTable(theDoc, ezFlexTable(x))
-# 
-# writeDoc(theDoc, "my.html")
-# setwd(cd)
-# 
-# 
-
-
-
-
-
-EzPlotter2 =
-  setRefClass("EzPlotter2",
-              fields = c("name", "ggplot", "helpText", "mouseOverText"),
-              methods = list(
-                initialize = function(ggplot, name=NULL)
-                {
-                  if (ezIsSpecified(name)){
-                    name <<- name
-                  } else {
-                    name <<- "EzPlotter2"
-                  }
-                  ggplot <<- ggplot
-                  helpText <<- "Default helpText"
-                  mouseOverText <<- "Default mouseOverText"
-                },
-                plotPng = function(file=NULL, width=480, height=480)
-                {
-                  "Creates a .png file of a plot."
-                  if (ezIsSpecified(file)) {
-                    filename = file
-                  } else {
-                    filename = paste(name, ".png", sep="")
-                  }
-                  png(filename = filename, width, height)
-                  plot(ggplot)
-                  dev.off()
-                  return(filename)
-                },
-                plotPdf = function(file=NULL, width=480, height=480)
-                {
-                  "Creates a .pdf file of a plot."
-                  if (ezIsSpecified(file)) {
-                    filename = file
-                  } else {
-                    filename = paste(name, ".pdf", sep="")
-                  }
-                  width = round(width/72, digits=2)
-                  height = round(height/72, digits=2)
-                  pdf(file = filename, width, height)
-                  plot(ggplot)
-                  dev.off()
-                  return(filename)
-                }
-              )
-  )
-
-ezImageFileLink2 = function(ggplot, file=NULL, mouseOverText=ezPlotter$mouseOverText,
-                              addPdfLink=TRUE, width=480, height=480){
-  
-  ezPlotter = EzPlotter2$new(ggplot)
-  
-  pngName = ezPlotter$plotPng(file=file, width=width, height=height)
-  if (addPdfLink) {
-    pdfName = ezPlotter$plotPdf(file=sub(".png$", ".pdf", file), width=width, height=height)
-    imgFilePot = pot(paste('<img src="', pngName, '" title="', mouseOverText, '"/>'),
-                     hyperlink = pdfName)
-  } else {
-    imgFilePot = pot(paste('<img src="', pngName, '" title="', mouseOverText, '"/>'))
-  }
-  return(as.html(imgFilePot))
-}
-
-cd2 = getwd()
+cd = getwd()
 setwdNew("./scratch")
+
 theDoc = bsdoc(title = 'My document')
 theDoc = addTitle(theDoc, "A title")
-myPlot = qplot(iris$Sepal.Length, iris$Sepal.Width)
-myLink = ezImageFileLink2(myPlot, file="test.png")
-theDoc = addParagraph(theDoc, myLink)
+irislink = ezImageFileLink(EzPlotterIris$new(name="Iris"),
+                    main="Iris sepal shape", xlab="Iris sepal length", ylab="Iris sepal width",
+                    width=600,height=600)
+theDoc = addParagraph(theDoc, irislink)
+types = data.frame(matrix(rep(1:10, each=10), 10))
+volcanolink = ezImageFileLink(EzPlotterVolcano$new(name="Volcano2", log2Ratio=1:100, pValue=rep(10^(-4:5), each=10)),
+                    xlim=c(0,100), ylim=c(-5,4), pch=16, isPresent=1:50, types=types,
+                    colors=rainbow(ncol(types)), legendPos="bottomleft",
+                    main="Volcano2")
+theDoc = addParagraph(theDoc, volcanolink)
+scascalink = ezImageFileLink(EzPlotterXYScatterScatter$new(name="ScatterScatter", xVec=1:10, yVec=1:10),
+                    main="ScatterScatter")
+theDoc = addParagraph(theDoc, scascalink)
+x = ezMatrix("", rows=1, cols=c("iris", "xy", "iris2"))
+x[1, "iris"] = ezImageFileLink(EzPlotterIris$new(name="Iris2"))
+x[1, "xy"] = ezImageFileLink(EzPlotterXYScatterScatter$new(name="ScatterScatterScatter", xVec=1:10, yVec=1:10))
+x[1, "iris2"] = ezImageFileLink(EzPlotterIris$new(name="Iris3"))
+theDoc = addFlexTable(theDoc, ezFlexTable(x))
+vier = ezImageFileLink(EzPlotterIris$new(name="Iris4"))
+fuenf = ezImageFileLink(EzPlotterIris$new(name="Iris5"))
+theDoc = addFlexTable(theDoc, ezFlexTable(cbind(vier,fuenf)))
+smoscafilink = ezImageFileLink(EzPlotterSmoothScatter$new(name="SmoothScatter", y=data.frame(a=1:10,b=21:30)), file="SmoothScatterplot.png", cex=2)
+theDoc = addParagraph(theDoc, smoscafilink)
+smoscafilink2 = ezImageFileLink(EzPlotterSmoothScatter$new(name="SmoothScatterMultiple", y=data.frame(a=1:10,b=21:30,c=90:81), x=51:60))
+theDoc = addParagraph(theDoc, smoscafilink2)
+scatterlink = ezImageFileLink(EzPlotterScatter$new(name="Scatter", y=data.frame(a=1:10,b=21:30)))
+theDoc = addParagraph(theDoc, scatterlink)
+scatterlink2 = ezImageFileLink(EzPlotterScatter$new(name="ScatterMultiple", y=data.frame(a=1:10,b=21:30,c=90:81), x=51:60))
+theDoc = addParagraph(theDoc, scatterlink2)
+allscatterlink = ezImageFileLink(EzPlotterAllPairScatter$new(x=matrix(1:10,5)))
+theDoc = addParagraph(theDoc, allscatterlink)
+allscatterlink2 = ezImageFileLink(EzPlotterAllPairScatter$new(name="allshallscatter", x=matrix(1:25,5)))
+theDoc = addParagraph(theDoc, allscatterlink2)
+
+theList = list()
+for (i in 1:4){
+  theList[[i]] = ezImageFileLink(EzPlotterIris$new(name=paste0("Iris",i+10)))
+}
+theDoc = addFlexTable(theDoc, ezFlexTable(rbind(theList[1:4])))
+
+testLink = "testLink.txt"
+theDoc = addParagraph(theDoc, pot(paste("<a href='", testLink, "' type='application/text'>", testLink, "</a>")))
+
+# x = ezMatrix("", rows=1, cols=sampleNames)
+# for (nm in sampleNames){
+#   x[1, nm] = ezImageFileLink(EzPlotterIris$new(name="Iris2", data[sm]))
+# }
+# theDoc = addFlexTable(theDoc, ezFlexTable(x))
+
 writeDoc(theDoc, "my.html")
-setwd(cd2)
-
-
-# EzPlotterVolcano$new(name="Volcano2", log2Ratio=1:100, pValue=rep(10^(-4:5), each=10)),
-#                     xlim=c(0,100), ylim=c(-5,4), pch=16, isPresent=1:50, types=types,
-#                     colors=rainbow(ncol(types)), legendPos="bottomleft",
-#                     main="Volcano2")
+setwd(cd)
 
 
 
-# volcanoPlot = qplot(x=1:100, y=)
-
-
-
-
-xlim=NULL
-ylim=NULL
-pch=16
-isPresent=NULL
-types=NULL
-colors=rainbow(ncol(types))
-legendPos="bottomright"
-cex.main=1.0
-cex=0.8
-yType="p-value"
-
-xValues = 1:100
-yValues = -log10(rep(10^(-4:5), each=10))
-if (is.null(xlim)){
-  lrMaxAbs = max(abs(xValues), na.rm=TRUE)
-  xm = min(lrMaxAbs, 5)
-  xlim = c(-xm, xm)
-  xValues = shrinkToRange(xValues, theRange = xlim)
-}
-if (is.null(ylim)){
-  ym = min(max(yValues, na.rm=TRUE), 10)
-  ylim = c(0, ym)
-  yValues = shrinkToRange(yValues, theRange=ylim)
-}
-par(pty="s")
-par(cex.main=cex.main, cex=cex)
-graphics::plot(xValues, yValues, pch=pch, xlim=xlim, ylim=ylim,
-               col="gray", xlab="log2 ratio", ylab=paste("-log10(", yType, ")" ,sep=""))
-if (!is.null(isPresent)){
-  points(xValues[isPresent], yValues[isPresent], col="black", pch=pch)
-}
-if (!is.null(types)){
-  for (j in 1:ncol(types)){
-    points(xValues[types[,j]], yValues[types[,j]], col=colors[j], pch=pch)
-  }
-  if (!is.null(legendPos)){
-    legend(legendPos, colnames(types), col=colors, cex=1.2, pch=20, bty="o", pt.bg="white")
-  }
-}
 
