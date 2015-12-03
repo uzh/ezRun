3dc644dfe9424fd1fa7f4dd5079a5095598c4cba
pdschmid <peter.schmid@ieu.uzh.ch>
2015-12-03 13:11:23 +0100

    fix test examples

--- tests/testthat/test_examples.R
+++ tests/testthat/test_examples.R
@@ -5,7 +5,7 @@ test_that("Function examples", {
   setwdNew("run_examples")
   devtools::run_examples()
   setwd(cwd)
-  ezSystem("rm -fr ./run_examples")
+  ezSystem(paste("rm -fr", system.file("./run_examples", package="ezRun", mustWork=TRUE)))
   remove1 = system.file("DESCRIPTION_head", package="ezRun", mustWork=TRUE)
   remove2 = system.file("extdata/genes.bed", package="ezRun", mustWork=TRUE)
   remove3 = system.file("extdata/genesWithPrespliced.gtf", package="ezRun", mustWork=TRUE)
