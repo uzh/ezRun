323bed3a562f946628975996ffd993c15aa5eda7
pdschmid <peter.schmid@ieu.uzh.ch>
2015-12-03 13:32:41 +0100

    another fix

--- tests/testthat/test_examples.R
+++ tests/testthat/test_examples.R
@@ -1,14 +1,15 @@
 context("Runs all function examples and cleans them up afterwards.")
 
+cwd = getwd()
+
 test_that("Function examples", {
-  cwd = getwd()
   setwdNew("run_examples")
   devtools::run_examples()
+  rm0 = normalizePath("tests/testthat/run_examples")
+  rm1 = system.file("DESCRIPTION_head", package="ezRun", mustWork=TRUE)
+  rm2 = system.file("extdata/genes.bed", package="ezRun", mustWork=TRUE)
+  rm3 = system.file("extdata/genesWithPrespliced.gtf", package="ezRun", mustWork=TRUE)
+  cmd = paste("rm -fr", rm0, rm1, rm2, rm3)
   setwd(cwd)
-  ezSystem(paste("rm -fr", system.file("./run_examples", package="ezRun", mustWork=TRUE)))
-  remove1 = system.file("DESCRIPTION_head", package="ezRun", mustWork=TRUE)
-  remove2 = system.file("extdata/genes.bed", package="ezRun", mustWork=TRUE)
-  remove3 = system.file("extdata/genesWithPrespliced.gtf", package="ezRun", mustWork=TRUE)
-  cmd = paste("rm -fr", remove1, remove2, remove3)
   ezSystem(cmd)
 })
