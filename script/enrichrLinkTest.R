a440730ef9e8a8787c933b38cf0b2a697c15355a
pdschmid <peter.schmid@ieu.uzh.ch>
2015-12-11 17:11:58 +0100

    now the enrichr link works properly in the example

--- script/enrichrLinkTest.R
+++ script/enrichrLinkTest.R
@@ -11,19 +11,21 @@ genes = c("Nsun3","Polrmt","Nlrx1","Sfxn5","Zc3h12c","Slc25a39","Arsg","Defb29",
 
 if (grepl("musculus", param$refBuild) | grepl("sapiens", param$refBuild)){
   genesList = paste(genes, collapse="\\n")
-  jsCall = paste0("enrich({list: '", genesList, "', popup: true});")
-  jsFile = system.file("extdata/enrichrFoo.js", package="ezRun", mustWork=TRUE)
+  jsCall = paste0('enrich({list: "', genesList, '", popup: true});')
+  jsFile = system.file("extdata/enrichr.js", package="ezRun", mustWork=TRUE)
   jsFunction = paste(scan(jsFile, what = "character", sep = "\n", quiet = TRUE))
   wholeJS = c(jsFunction, jsCall)
   
   doc = openBsdocReport()
-  setwdNew("./enrichr")
-  enrichrDoc = openBsdocReport()
-  enrichrDoc = addJavascript(enrichrDoc, text=wholeJS)
-  addParagraph(enrichrDoc, as.html(pot("Go back", hyperlink="../testEnrichrLink.html")))
-  closeBsdocReport(enrichrDoc, "enrichr.html")
-  setwd("..")
+#   setwdNew("./enrichr")
+#   enrichrDoc = openBsdocReport()
+#   enrichrDoc = addJavascript(enrichrDoc, jsFile)
+#   addParagraph(enrichrDoc, as.html(pot("Go back", hyperlink="../testEnrichrLink.html")))
+#   closeBsdocReport(enrichrDoc, "enrichr.html")
+#   setwd("..")
+  addJavascript(doc, jsFile)
   addParagraph(doc, as.html(pot("Enrichr", hyperlink="enrichr/enrichr.html")))
+  addParagraph(doc, pot(paste0("<a href='javascript:void(0)' onClick='", jsCall, "'>blabla</a>")))
   closeBsdocReport(doc, "testEnrichrLink.html")
 }
 
