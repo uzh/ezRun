f4d62915fa293be4671c146d5c40181157685895
pdschmid <peter.schmid@ieu.uzh.ch>
2015-12-11 14:27:16 +0100

    update

--- script/enrichrLinkTest.R
+++ script/enrichrLinkTest.R
@@ -12,11 +12,14 @@ genes = c("Nsun3","Polrmt","Nlrx1","Sfxn5","Zc3h12c","Slc25a39","Arsg","Defb29",
 if (grepl("musculus", param$refBuild) | grepl("sapiens", param$refBuild)){
   setwdNew("./enrichr")
   enrichrDoc = openBsdocReport()
-  enrichJavaFile = system.file("extdata/enrichrFoo.java", package="ezRun", mustWork=TRUE)
-  addJavascript(enrichrDoc, enrichJavaFile)
+  javaFile = system.file("extdata/enrichrFoo.js", package="ezRun", mustWork=TRUE)
+  # addJavascript(enrichrDoc, javaFile)
+#   javaObject = rJava::.jnew("java/awt/Frame", "Hello")
+#   rJava::.jcall(javaObject, "V", "addJavascript", javaFile)
   genesList = paste(genes, collapse="\n")
   plotCmd = expression({
     ezLegend(title="Enrichr link")
+    javaCall
     add.plot.interactivity(text, labels="click me", x=1, y=0.8, click.actions=paste0("enrich({list: ", genesList, ", popup: true});"))
   })
   enrichrLink = ezImageFileLink(plotCmd, file="enrichrLink.png", width=70, height=40, addPdfLink=FALSE)
@@ -26,6 +29,13 @@ if (grepl("musculus", param$refBuild) | grepl("sapiens", param$refBuild)){
 }
 
 # maybe with cmd line
-cmd = paste0("java ", enrichJavaFile, " 'enrich({list: ", genesList, ", popup: true});'")
+cmd = paste0('javac ', enrichJavaFile, ' "enrich({list: ', genesList, ', popup: true});"')
 ezSystem(cmd)
+
+
+ezSystem(paste0('java -jar js.jar ', enrichJavaFile))
+
+
+
+
 
