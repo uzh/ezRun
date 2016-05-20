cb7c15013203033fa8234c29b4cfcaeadae9c382
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2016-05-20 17:52:44 +0200

    fix

--- R/twoGroups.R
+++ R/twoGroups.R
@@ -353,7 +353,8 @@ writeNgsTwoGroupReport = function(dataset, deResult, output, htmlFile="00index.h
   deResult$saveToFile(resultObjFile)
   addParagraph(doc, ezPot("Explore result interactively",
                           hyperlink = paste0("http://fgcz-176.uzh.ch/shiny/fgcz_rnaSeqInteractiveReport_app/?data=",
-                                             file.path(output$getColumn("Report"), resultObjFile))))
+                                             file.path(output$getColumn("Report"), resultObjFile)),
+                          linkTarget = "_blank"))
   ## TODO: add the link to the shiny-app
   
   logSignal = log2(shiftZeros(result$xNorm, param$minSignal))
