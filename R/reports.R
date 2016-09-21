79729b138d366fa6b9ab25555e010482e0657e7c
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2016-09-21 13:57:04 +0200

    disable addTableWhite

--- R/reports.R
+++ R/reports.R
@@ -499,18 +499,18 @@ ezAddTable = function(doc, x, bgcolors=NULL, valign="middle", border=1, head="")
   addFlexTable(doc, table)
 }
 
-## NOTEP: used once in gage-reports.R, but that needs to be refactored anyway.
-##' @describeIn ezAddTable Does the same with a white font and returning the table instead of adding it to the document.
-ezAddTableWhite = function(x, bgcolors=NULL, valign="middle", border=1, head=""){
-  if (is.null(bgcolors)){
-    bgcolors = matrix("#ffffff", nrow=nrow(x), ncol=ncol(x))
-  }
-  ##x = cbind(rownames(x),x)
-  x = as.html(pot(paste('<font color="white">', x, '</font>')))
-  bodyCells = cellProperties(border.width=border, vertical.align=valign)
-  table = FlexTable(x, header.columns = FALSE, body.cell.props=bodyCells,
-                    header.cell.props=cellProperties(border.width = border))
-  table = setFlexTableBackgroundColors(table, j=1:ncol(x), colors=bgcolors)
-  table = addHeaderRow(table, colnames(x))
-  return(table)
-}
+# ## NOTEP: used once in gage-reports.R, but that needs to be refactored anyway.
+# ##' @describeIn ezAddTable Does the same with a white font and returning the table instead of adding it to the document.
+# ezAddTableWhite = function(x, bgcolors=NULL, valign="middle", border=1, head=""){
+#   if (is.null(bgcolors)){
+#     bgcolors = matrix("#ffffff", nrow=nrow(x), ncol=ncol(x))
+#   }
+#   ##x = cbind(rownames(x),x)
+#   x = as.html(pot(paste('<font color="white">', x, '</font>')))
+#   bodyCells = cellProperties(border.width=border, vertical.align=valign)
+#   table = FlexTable(x, header.columns = FALSE, body.cell.props=bodyCells,
+#                     header.cell.props=cellProperties(border.width = border))
+#   table = setFlexTableBackgroundColors(table, j=1:ncol(x), colors=bgcolors)
+#   table = addHeaderRow(table, colnames(x))
+#   return(table)
+# }
