225ab9ddeb15588e0997532b7de9773e5480985b
Lennart Opitz <lopitz@fgcz.ethz.ch>
2016-11-10 21:45:00 +0100

    update genotypingApp

--- R/app-joinGenoTypes.R
+++ R/app-joinGenoTypes.R
@@ -9,52 +9,64 @@ ezMethodJoinGenoTypes = function(input=NA, output=NA, param=NA){
   setwdNew(param[['name']])
   dataset = input$meta
   dataset[['GVCF [File]']] = input$getFullPaths("GVCF")
-  genomeSeq = param$ezRef["refFastaFile"]
-  species = limma::strsplit2(param$ezRef['refBuild'],'/')[1]
-  knownSites = list.files(param$ezRef["refVariantsDir"],pattern='vcf$',full.names = T) 
-  dbsnpFile = knownSites[grep('dbsnp.*vcf$', knownSites)]
-  ExAcFile = knownSites[grep('ExAC.*vcf$', knownSites)]
-  snpEffConfig = file.path(dirname(param$ezRef["refFeatureFile"]),"snpEff/snpEff.config")
-  snpEffDB = basename(list.dirs(dirname(snpEffConfig))) ####bad Hack
-  snpEffDB = snpEffDB[grep(species, snpEffDB)]
-  javaCall = paste0(JAVA, " -Djava.io.tmpdir=. -Xmx", param$ram, "g")
   datasetCaseList = split(dataset,input$getColumn(param$grouping))
-  for (i in 1:length(datasetCaseList)) {
-    datasetCase = datasetCaseList[[i]]
+  if(is.null(param$mc.cores)){
+    param[['mc.cores']] = 1
+  }
+  param[['genomeSeq']] = param$ezRef["refFastaFile"]
+  param[['species']] = limma::strsplit2(param$ezRef['refBuild'],'/')[1]
+  param[['knownSites']] = list.files(param$ezRef["refVariantsDir"],pattern='vcf.gz$',full.names = T) 
+  param[['dbsnpFile']] = param$knownSites[grep('dbsnp.*vcf.gz$', param$knownSites)]
+  param[['snpEffConfig']] = file.path(dirname(param$ezRef["refFeatureFile"]),"snpEff/snpEff.config")
+  snpEffDB = basename(list.dirs(dirname(param$snpEffConfig))) ####bad Hack
+  param[['snpEffDB']] = snpEffDB[grep(param$species, snpEffDB)]
+  param[['javaCall']] = paste0(JAVA, " -Djava.io.tmpdir=. -Xmx", param$ram/param$mc.cores, "g")
+  
+  require(parallel)
+  ezMclapply(datasetCaseList,runGatkPipeline, param, mc.cores = param$mc.cores)
+  ezSystem('rm *.tranches *recal* *_annotated.vcf*')
+  #Further processing for Kispi-Samples ->make Report per Family (per flag)
+  return("Success")
+}
+
+runGatkPipeline = function(datasetCase, param=NA){
+    caseName = unique(datasetCase[[paste(param$grouping,'[Factor]')]])
+    myLog = paste0('log_',caseName,'.txt')
     fileCmd = vector(mode = 'character',length = 1)
     for (j in 1:nrow(datasetCase)){
       fileCmd = paste(fileCmd,paste("--variant", datasetCase[['GVCF [File]']][j], collapse=','))
     }  
     
-    GenotypeGVCF = paste(javaCall,"-jar", GATK_JAR, " -T GenotypeGVCFs")
-    outputFile = paste0(names(datasetCaseList)[i],'.vcf')
-    cmd = paste(GenotypeGVCF, "-R", genomeSeq,
+    GenotypeGVCF = paste(param$javaCall,"-jar", GATK_JAR, " -T GenotypeGVCFs")
+    outputFile = paste0(caseName,'.vcf')
+    cmd = paste(GenotypeGVCF, "-R", param$genomeSeq,
                 fileCmd,
+                "--dbsnp", param$dbsnpFile,
                 "-o", outputFile,
-                "-nt", param$cores)
+                "-nt", round(param$cores/param$mc.cores))
     if(!is.null(param$targetFile)){
       cmd = paste(cmd,
                   "-L", param$targetFile)
     }
-    ezSystem(cmd)
+    ezSystem(paste(cmd,'2>',myLog))
     
-    if(species == 'Homo_sapiens'){
-      VariantRecalibrator1 = paste(javaCall,"-jar", GATK_JAR, " -T VariantRecalibrator")
-      hapmapFile = knownSites[grep('hapmap_.*vcf$', knownSites)]
-      h1000G_omniFile = knownSites[grep('1000G_omni.*vcf$', knownSites)]
-      h1000G_phase1File = knownSites[grep('1000G_phase1.snps.*vcf$', knownSites)]
+    if(param$species == 'Homo_sapiens'){
+      VariantRecalibrator1 = paste(param$javaCall,"-jar", GATK_JAR, " -T VariantRecalibrator")
+      hapmapFile = param$knownSites[grep('hapmap_.*vcf.gz$', param$knownSites)]
+      h1000G_omniFile = param$knownSites[grep('1000G_omni.*vcf.gz$', param$knownSites)]
+      h1000G_phase1File = param$knownSites[grep('1000G_phase1.snps.*vcf.gz$', param$knownSites)]
       
-      cmd = paste(VariantRecalibrator1, "-R", genomeSeq,
+      cmd = paste(VariantRecalibrator1, "-R", param$genomeSeq,
                   "-input", outputFile,
                   "-resource:hapmap,known=false,training=true,truth=true,prior=15.0", hapmapFile,
                   "-resource:omni,known=false,training=true,truth=false,prior=12.0",  h1000G_omniFile,
                   "-resource:1000G,known=false,training=true,truth=false,prior=10.0", h1000G_phase1File,
-                  "-resource:dbsnp,known=true,training=false,truth=false,prior=2.0",  dbsnpFile,
+                  "-resource:dbsnp,known=true,training=false,truth=false,prior=2.0",  param$dbsnpFile,
                   "-an QD -an FS -an MQRankSum -an ReadPosRankSum -an MQ",
                   "-mode SNP",
-                  "-recalFile raw.SNPs.recal",
-                  "-tranchesFile raw.SNPs.tranches",
-                  "-rscriptFile recal.SNPs.plots.R")
+                  "-recalFile", paste0(caseName,'_raw.SNPs.recal'),
+                  "-tranchesFile",paste0(caseName,'_raw.SNPs.tranches'),
+                  "-rscriptFile", paste0(caseName,'_recal.SNPs.plots.R'))
       
       if(!is.null(param$targetFile)){
         cmd = paste(cmd,
@@ -62,93 +74,87 @@ ezMethodJoinGenoTypes = function(input=NA, output=NA, param=NA){
       } else {
         cmd = paste(cmd, "-an DP")
       }
-      ezSystem(cmd)
+      ezSystem(paste(cmd,'2>>',myLog))
       
       #Apply RecalibrationOutput for SNPs:
-      VariantRecalibrator2 = paste(javaCall,"-jar", GATK_JAR, "-T ApplyRecalibration")
-      cmd = paste(VariantRecalibrator2, "-R", genomeSeq,
+      VariantRecalibrator2 = paste(param$javaCall,"-jar", GATK_JAR, "-T ApplyRecalibration")
+      cmd = paste(VariantRecalibrator2, "-R", param$genomeSeq,
                   "-input", outputFile,
                   "-mode SNP",
-                  "-recalFile raw.SNPs.recal",
-                  "-tranchesFile raw.SNPs.tranches",
-                  "-o recal.SNPs.vcf",
+                  "-recalFile", paste0(caseName,'_raw.SNPs.recal'),
+                  "-tranchesFile",paste0(caseName,'_raw.SNPs.tranches'),
+                  "-o",paste0(caseName,'_recal.SNPs.vcf'),
                   "-ts_filter_level 99.0",
-                  "-nt", param$cores)
+                  "-nt", round(param$cores/param$mc.cores))
       if(!is.null(param$targetFile)){
         cmd = paste(cmd,
                     "-L", param$targetFile)
       } 
-      ezSystem(cmd)
-      ezSystem(paste("mv raw.SNPs.tranches.pdf", paste0(names(datasetCaseList)[i],"_raw.SNPs.tranches.pdf")))
+      ezSystem(paste(cmd,'2>>',myLog))
       
       #2.Run VariantRecalibration for InDels:
-      millsFile = knownSites[grep('Mills.*vcf$', knownSites)]
-      cmd = paste(VariantRecalibrator1, "-R", genomeSeq,
-                  "-input recal.SNPs.vcf",
+      millsFile = param$knownSites[grep('Mills.*vcf.gz$', param$knownSites)]
+      cmd = paste(VariantRecalibrator1, "-R", param$genomeSeq,
+                  "-input", paste0(caseName,"_recal.SNPs.vcf"),
                   "-resource:mills,known=false,training=true,truth=true,prior=12.0", millsFile,
                   "--maxGaussians 4 --minNumBadVariants 100 --max_attempts 3", #might be suboptimal
                   "-an QD -an FS -an MQRankSum -an ReadPosRankSum -an MQ",
                   "-mode INDEL",
-                  "-recalFile raw.InDels.recal",
-                  "-tranchesFile raw.InDels.tranches",
-                  "-rscriptFile recal.InDels.plots.R")
+                  "-recalFile", paste0(caseName,"_raw.InDels.recal"),
+                  "-tranchesFile", paste0(caseName,"_raw.InDels.tranches"),
+                  "-rscriptFile", paste0(caseName,"_recal.InDels.plots.R"))
       if(!is.null(param$targetFile)){
         cmd = paste(cmd,
                     "-L", param$targetFile)
       } else {
         cmd = paste(cmd, "-an DP")
       }
-      ezSystem(cmd)
+      ezSystem(paste(cmd,'2>>',myLog))
       
       #Apply RecalibrationOutput for InDels:
-      VariantRecalibrator2 = paste(javaCall,"-jar", GATK_JAR, "-T ApplyRecalibration")
-      cmd = paste(VariantRecalibrator2, "-R", genomeSeq,
-                  "-input recal.SNPs.vcf",
+      VariantRecalibrator2 = paste(param$javaCall,"-jar", GATK_JAR, "-T ApplyRecalibration")
+      cmd = paste(VariantRecalibrator2, "-R", param$genomeSeq,
+                  "-input", paste0(caseName,"_recal.SNPs.vcf"),
                   "-mode INDEL",
-                  "-recalFile raw.InDels.recal",
-                  "-tranchesFile raw.InDels.tranches",
+                  "-recalFile",paste0(caseName,"_raw.InDels.recal"),
+                  "-tranchesFile",paste0(caseName,"_raw.InDels.tranches"),
                   "-o ",outputFile,
                   "-ts_filter_level 99.0",
-                  "-nt", param$cores)
+                  "-nt", round(param$cores/param$mc.cores))
       if(!is.null(param$targetFile)){
         cmd = paste(cmd,
                     "-L", param$targetFile)
       } 
-     ezSystem(cmd)
-    }
-    #1.dbSnp-Annotation/ExAc-Annotation:
-    cmd = paste(javaCall, "-jar", file.path(SNPEFF_DIR, "SnpSift.jar"), "annotate -id", dbsnpFile, outputFile, ">", paste0(outputFile, "_annotated.vcf"))
-    ezSystem(cmd)
-    if(species == 'Homo_sapiens'){
-      ezSystem(paste("sed -i 's/;AF=/;Case_AF=/g' ", paste0(outputFile, "_annotated.vcf")))
-      ezSystem(paste("sed -i 's/\tAC=/\tCase_AC=/g' ",paste0(outputFile, "_annotated.vcf")))
-      ezSystem(paste("sed -i 's/;AN=/;Case_AN=/g' ",paste0(outputFile, "_annotated.vcf")))
-      cmd = paste(javaCall, "-jar", file.path(SNPEFF_DIR, "SnpSift.jar"), "annotate -noId -info AF,AC,AN", ExAcFile, paste0(outputFile, "_annotated.vcf"), ">", outputFile)
-      ezSystem(cmd)
-    } else {
-      ezSystem(paste("mv",paste0(outputFile, "_annotated.vcf"), outputFile))
-    }
-    #2.SnpEff:
-    htmlOutputFile = paste0(names(datasetCaseList)[i],'.html')
-    cmd = paste(javaCall, "-jar", file.path(SNPEFF_DIR, "snpEff.jar"), "-s", htmlOutputFile, "-c", snpEffConfig, snpEffDB, "-v", outputFile,">", 
-                paste0(outputFile, "_annotated.vcf"))
-    ezSystem(cmd)
+      ezSystem(paste(cmd,'2>>',myLog))
+      #Add ExAc-Annotation:
+      ExAcFile = param$knownSites[grep('ExAC.*vcf.gz$', param$knownSites)]
+      VariantAnnotation = paste(param$javaCall,"-jar", GATK_JAR, "-T VariantAnnotator")
+      cmd = paste(VariantAnnotation, "-R", param$genomeSeq,
+                  "--resource:ExAC",  ExAcFile,
+                  "-o ",paste0(outputFile, "_annotated.vcf"),
+                  "--expression ExAC.AF",
+                  "--expression ExAC.AC",
+                  "--expression ExAC.AN",
+                  "--expression ExAC.AC_Het",
+                  "--expression ExAC.AC_Hom",
+                  "-V", outputFile,
+                  "-nt", round(param$cores/param$mc.cores)) 
+      if(!is.null(param$targetFile)){
+        cmd = paste(cmd,
+                    "-L", param$targetFile)
+      } 
+      ezSystem(paste(cmd,'2>>',myLog))
+      cmd = paste(param$javaCall, "-jar", file.path(SNPEFF_DIR, "SnpSift.jar"), "dbnsfp -f 1000Gp1_EUR_AF,Uniprot_acc,Interpro_domain,phastCons100way_vertebrate,CADD_phred,Polyphen2_HDIV_pred,Polyphen2_HVAR_pred,SIFT_score,SIFT_pred -v -db /srv/GT/databases/dbNSFP/dbNSFP2.9.txt.gz",paste0(outputFile, "_annotated.vcf"), ">", outputFile)
+      ezSystem(paste(cmd,'2>>',myLog))
+    } 
     
-    #3.Add SIFT & CO Annotation if Human (dbnsfp):
-    if(species == 'Homo_sapiens'){
-      cmd = paste(javaCall, "-jar", file.path(SNPEFF_DIR, "SnpSift.jar"), "dbnsfp -f SIFT_score,SIFT_pred,Polyphen2_HDIV_pred,1000Gp1_EUR_AF,CADD_phred -v -db /srv/GT/databases/dbNSFP/dbNSFP2.9.txt.gz",paste0(outputFile, "_annotated.vcf"), ">", outputFile)
-      ezSystem(cmd)
-    } else {
-      ezSystem(paste("mv",paste0(outputFile, "_annotated.vcf"), outputFile))
+    ezSystem(paste("mv",outputFile,paste0(outputFile, "_annotated.vcf")))
+    #SnpEff:
+    htmlOutputFile = paste0(caseName,'.html')
+    cmd = paste(param$javaCall, "-jar", file.path(SNPEFF_DIR, "snpEff.jar"), "-s", htmlOutputFile, "-c", param$snpEffConfig, param$snpEffDB, "-v", paste0(outputFile, "_annotated.vcf"),">", 
+                outputFile)
+    ezSystem(paste(cmd,'2>>',myLog))
     }
-  }
-  ezSystem('rm *.tranches *recal* *_annotated.vcf')
-  
-  
-  
-  #Further processing for Kispi-Samples ->make Report per Family (per flag)
-  return("Success")
-}
 
 
 ##' @template app-template
@@ -164,7 +170,7 @@ EzAppJoinGenoTypes <-
                   runMethod <<- ezMethodJoinGenoTypes
                   name <<- "EzAppJoinGenoTypes"
                   appDefaults <<- rbind(targetFile = ezFrame(Type="character",  DefaultValue="", Description="restrict to targeted genomic regions"),
-                                        vcfFilt.minAltCount = ezFrame(Type="integer",  DefaultValue=8,  Description="minimum coverage for the alternative variant"),
+                                        vcfFilt.minAltCount = ezFrame(Type="integer",  DefaultValue=3,  Description="minimum coverage for the alternative variant"),
                                         vcfFilt.minReadDepth = ezFrame(Type="integer",  DefaultValue=20,  Description="minimum read depth"))
                 }
               )
