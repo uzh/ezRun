beb1bbdfe2cdaabe13eab91253d900df745eb97c
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2017-09-26 10:08:36 +0200

    fix defaults

--- R/00defaults.R
+++ R/00defaults.R
@@ -44,6 +44,14 @@ ezParam = function(userParam=list(), globalDefaults=getGlobalDefaults(), appDefa
                          logical=as.logical(value),
                          stop("unsupported type: ", defaults[nm, "Type"]))
   }
+  ## avoid special characters in any option
+  lapply(userParam, function(optString){
+    if (any(grepl("[;\\(){}$%#!*]", optString))){
+      stop("special characters not allowed in option string: ", optString)
+    }
+  })
+  
+  
   # we build the ezRef object from hints in the general parameters
   if (is.null(userParam$ezRef)){
     userParam$ezRef = EzRef(userParam)
@@ -69,7 +77,7 @@ parseOptions = function(optString){
   if (length(grep("[[:cntrl:]]", optString))> 0){
     stop("control characters not allowed in option string")
   }
-  if (length(grep("[;\\(){}$%:#!?*]", optString))> 0){
+  if (any(grepl("[;\\(){}$%#!*]", optString))){
     stop("special characters not allowed in option string")
   }
   params = unlist(strsplit(optString, " ", fixed=TRUE))
