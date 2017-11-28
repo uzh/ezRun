25d05a0d00fff963fe6ff5012f2740e4a84a55b4
Giancarlo Russo <grusso@fgcz-net.unizh.ch>
2017-11-29 00:45:55 +0100

    mothur data clean update

--- R/app-MothurDataClean.R
+++ R/app-MothurDataClean.R
@@ -9,33 +9,44 @@
 ezMethodMothurDataClean = function(input=NA, output=NA, param=NA, 
                           htmlFile="00index.html"){
   require(rmarkdown)
-  mothurExe = "/usr/local/ngseq/src/mothur-1.39.5/mothur"
-  mothurBatchFile = "/home/grusso/Rcodes/giancarlo/genericScipts/mothurMiSeqSOPtest.batch"
-  mothurInput = "datasetNoHeaderForMothur.tsv"
-  
+  require(ShortRead)
+  require(phyloseq)
+  require(plyr)
+  require(ape)
+  require(ggplot2)
+#  mothurExe = "/usr/local/ngseq/src/mothur-1.39.5/mothur"
+#  mothurBatchFile = "/home/grusso/Rcodes/giancarlo/genericScipts/mothurSingleEndCleanApp.batch"
   setwdNew(basename(output$getColumn("Static Report")))
-  ## remove header to creat input files for mothur and copy it 
-  cmdNoHead = paste("grep -v Name", "dataset.tsv", ">", mothurInput)
-  ezSystem(cmdNoHead)
-  cmdMothur = paste(mothurExe,mothurBatchFile)
-  ezSystem(cmdMothur)
-  
-  ## Copy the style files and templates
+  dataset = input$meta
+  ### read fastq files and prepare inputs for Mothur
+  datasetToMothur(dataset)
+
+### update batch file pacbio with parameters and run mothur
+updateBatchCmdPacbio <- paste0("sed -e s/\"MIN_LEN\"/", param$minLen_PacBio, "/g",
+                               " -e s/\"MAX_LEN\"/", param$maxLen_PacBio, "/g",
+                               " -e s/\"CUTOFF\"/", param$cutOff, "/g",
+                               " -e s/\"DIFFS\"/", param$diffs_PacBio, "/g",
+                               " -e s/\"Mothur\"/\"PacBio\/g",
+                               MOTHUR_DATA_CLEAN_BATCH, " >", MOTHUR_DATA_CLEAN_BATCH_PACBIO)
+ezSystem(updateBatchCmdPacbio)
+cmdMothurPacBio = paste(MOTHUR_EXE,MOTHUR_DATA_CLEAN_BATCH_PACBIO)
+ezSystem(cmdMothurPacBio)
+### update batch file Illumina with parameters and run mothur
+updateBatchCmdPacbio <- paste0("sed -e s/\"MIN_LEN\"/", param$minLen_Illumina, "/g",
+                               " -e s/\"MAX_LEN\"/", param$maxLen_Illumina, "/g",
+                               " -e s/\"CUTOFF\"/", param$cutOff, "/g",
+                               " -e s/\"DIFFS\"/", param$diffs_Illumina, "/g",
+                               " -e s/\"Mothur\"/\"Illumina\/g",
+                               MOTHUR_DATA_CLEAN_BATCH, " >", MOTHUR_DATA_CLEAN_BATCH_ILLUMINA)
+ezSystem(updateBatchCmdIllumina)
+cmdMothurPacBio = paste(MOTHUR_EXE,MOTHUR_DATA_CLEAN_BATCH_ILLUMINA)
+ezSystem(cmdMothurIllumina)
+
+## Copy the style files and templates
   styleFiles <- file.path(system.file("templates", package="ezRun"),
-                          c("fgcz.css", "FastQC.Rmd", "FastQC_overview.Rmd",
+                          c("fgcz.css", "MothurDataClean.Rmd",
                             "fgcz_header.html", "banner.png"))
   file.copy(from=styleFiles, to=".", overwrite=TRUE)
-  
-  plots = c("Summary of sequences in each sample.png",
-            "Per sequence quality scores"="per_sequence_quality.png",
-            "Per tile sequence quality"="per_tile_quality.png",
-            "Per base sequence content"="per_base_sequence_content.png",
-            "Per sequence GC content"="per_sequence_gc_content.png",
-            "Per base N content"="per_base_n_content.png",
-            "Sequence Length Distribution"="sequence_length_distribution.png",
-            "Sequence Duplication Levels"="duplication_levels.png",
-            "Adapter Content"="adapter_content.png",
-            "Kmer Content"="kmer_profiles.png")
 }
  
 ##' @template app-template
@@ -51,7 +62,15 @@ EzAppMothurDataClean <-
                   "Initializes the application using its specific defaults."
                   runMethod <<- ezMethodMothurDataClean
                   name <<- "EzAppMothurDataClean"
-                  appDefaults <<- rbind(cutOff = ezFrame(Type="numeric",  DefaultValue="0,03",Description="Cut-off for OTU clustering.")
+                  appDefaults <<- rbind(cutOff = ezFrame(Type="numeric",  DefaultValue="80",Description="Cut-off for taxonomy assignment"),
+                                        diffs_Illumina = ezFrame(Type="integer",  DefaultValue="2",Description="Differences allowed in the pre.cluster step. 
+                                                                 Should be 1 every 100 bases"),
+                                        diffs_PacBio = ezFrame(Type="integer",  DefaultValue="15",Description="Differences allowed in the pre.cluster step. 
+                                                               Should be 1 every 100 bases"),
+                                        minLen_Illumina = ezFrame(Type="integer",  DefaultValue="290",Description="Min length Illumina"),     
+                                        maxLen_Illumina = ezFrame(Type="integer",  DefaultValue="290",Description="Max length Illumina"),
+                                        minLen_PacBio = ezFrame(Type="integer",  DefaultValue="290",Description="Min length PacBio"),
+                                        maxLen_PacBio = ezFrame(Type="integer",  DefaultValue="290",Description="Max length PacBio"),
                   )
                 }
               )
