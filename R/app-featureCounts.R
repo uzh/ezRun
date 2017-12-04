6d6e137321c2c3e7b8fc8b514289132b153315e4
Ge Tan <gtan@me.com>
2017-12-04 16:05:12 +0100

    fix the renaming bug when RG starts with numbers

--- R/app-featureCounts.R
+++ R/app-featureCounts.R
@@ -301,7 +301,13 @@ ezMethodSingleCellFeatureCounts <- function(input=NA, output=NA, param=NA){
                                colnames(countsFixed))
   tagsRG <- sub("ID:", "", 
                 sapply(bamHeaders[[1]]$text[names(bamHeaders[[1]]$text) == "@RG"], "[", 1))
+  ## RG starts with numbers will have X after make.names
+  ## But featureCounts doesn't have this X.
   fixNameMapping <- setNames(tagsRG, make.names(tagsRG))
+  indexStartNumber <- grep("^\\d", fixNameMapping)
+  names(fixNameMapping)[indexStartNumber] <- sub("^X", "", 
+                                                 names(fixNameMapping)[indexStartNumber])
+  }
   colnames(countsFixed) <- fixNameMapping[colnames(countsFixed)]
   
   ## wirteMM doesn't hold the colnames and rownames in mtx
