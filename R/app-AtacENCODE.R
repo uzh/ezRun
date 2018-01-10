bcf548abe9ef20ce279b127a178019a4c1f059a0
Ge Tan <gtan@me.com>
2018-01-10 14:12:38 +0100

    atac ENCODE app

---
+++ R/app-AtacENCODE.R
@@ -0,0 +1,56 @@
+###################################################################
+# Functional Genomics Center Zurich
+# This code is distributed under the terms of the GNU General
+# Public License Version 3, June 2007.
+# The terms are available here: http://www.gnu.org/licenses/gpl.html
+# www.fgcz.ch
+
+### ATAC-seq ENCODE pipeline
+### https://github.com/kundajelab/atac_dnase_pipelines
+
+EzAppAtacENCODE <-
+  setRefClass("EzAppAtacENCODE",
+              contains = "EzApp",
+              methods = list(
+                initialize = function()
+                {
+                  "Initializes the application using its specific defaults."
+                  runMethod <<- ezMethodAtacENCODE
+                  name <<- "EzAppAtacENCODE"
+                }
+              )
+  )
+
+ezMethodAtacENCODE <- function(input=NA, output=NA, param=NA, 
+                               htmlFile="00index.html"){
+  
+  ## Environments for this pipeline
+  setEnvironments("conda")
+  Sys.setenv("_JAVA_OPTIONS"="-Xms256M -Xmx728M -XX:ParallelGCThreads=1")
+  
+  setwdNew(basename(output$getColumn("Report")))
+  dataset = input$meta
+  samples = rownames(dataset)
+  
+  ## This pipeline only supports human and mouse
+  if(grepl("(Homo|human)", param$Species, ignore.case = TRUE)){
+    refBuild <- "hg38"
+  }else if(grepl("(Mus|mouse)", param$Species, ignore.case = TRUE)){
+    refBuild <- "mm10"
+  }else{
+    stop("Only human and mouse are supported by this pipeline!")
+  }
+  
+  ## We only work on paired-end data for now; although the pipeline supporst SE
+  fastqR1Fns <- input$getFullPaths("Read1")
+  fastqR2Fns <- input$getFullPaths("Read2")
+  
+  cmd <- paste(paste0("-fastq", 1:length(fastqR1Fns), "_1"), fastqR1Fns,
+               paste0("-fastq", 1:length(fastqR1Fns), "_2"), fastqR2Fns)
+  cmd <- paste0(cmd, collapse=" ")
+  cmd <- paste("bds", ATACENCODE, "-species", refBuild, 
+               "-auto_detect_adapter -nth", param$cores, cmd)
+  ezSystem(cmd)
+  return("Success")
+}
+
