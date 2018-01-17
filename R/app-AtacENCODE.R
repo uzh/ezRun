e7bd0ad07cc348936a0290ccf066a4a2946f2f88
Ge Tan <gtan@me.com>
2018-01-17 13:12:26 +0100

    rerun the ATAC ENCODE pipeline until it succeeds

--- R/app-AtacENCODE.R
+++ R/app-AtacENCODE.R
@@ -53,7 +53,12 @@ ezMethodAtacENCODE <- function(input=NA, output=NA, param=NA,
   cmd <- paste0(cmd, collapse=" ")
   cmd <- paste("bds", ATACENCODE, "-species", refBuild, 
                "-auto_detect_adapter -nth", param$cores, cmd)
-  ezSystem(cmd)
+  ## This ATAC ENCODE pipeline can fail randomly. 
+  ## We force it to rerun until it succeed.
+  status <- try(ezSystem(cmd))
+  while(class(status) == "try-error"){
+    status <- try(ezSystem(cmd))
+  }
   return("Success")
 }
 
