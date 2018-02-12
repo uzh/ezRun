7e0a4bbb220cfe3cc3e04a217ae76f9bbe9520cd
Ge Tan <gtan@me.com>
2018-02-12 22:00:47 +0100

    Skeleton of SCCountQC App

---
+++ R/app-SCCountQC.R
@@ -0,0 +1,25 @@
+###################################################################
+# Functional Genomics Center Zurich
+# This code is distributed under the terms of the GNU General
+# Public License Version 3, June 2007.
+# The terms are available here: http://www.gnu.org/licenses/gpl.html
+# www.fgcz.ch
+
+EzAppSCCountQC <-
+  setRefClass("EzAppSCCountQC",
+              contains = "EzApp",
+              methods = list(
+                initialize = function()
+                {
+                  "Initializes the application using its specific defaults."
+                  runMethod <<- ezMethodSCCountQC
+                  name <<- "EzAppSCCountQC"
+                  appDefaults <<- rbind(runGO=ezFrame(Type="logical", DefaultValue=TRUE, Description="whether to run the GO analysis"))
+                }
+              )
+  )
+
+ezMethodSCCountQC = function(input=NA, output=NA, param=NA, 
+                             htmlFile="00index.html"){
+
+}
