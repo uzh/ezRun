d09324365ea5517f73b8fca692b02ddf439faf94
Ge Tan <gtan@me.com>
2018-02-20 15:25:53 +0100

    SCCountsQC: alignment metrics

---
+++ R/Picard.R
@@ -0,0 +1,25 @@
+###################################################################
+# Functional Genomics Center Zurich
+# This code is distributed under the terms of the GNU General
+# Public License Version 3, June 2007.
+# The terms are available here: http://www.gnu.org/licenses/gpl.html
+# www.fgcz.ch
+
+CollectAlignmentSummaryMetrics <- function(inBam, fastaFn, 
+                                           metricLevel=c("ALL_READS", "SAMPLE",
+                                                         "LIBRARY", "READ_GROUP")){
+  setEnvironments("picard")
+  outputFn <- tempfile(pattern="CollectAlignmentSummaryMetrics",
+                       fileext=".txt")
+  cmd <- paste("java -jar", Sys.getenv("Picard_jar"),
+               "CollectAlignmentSummaryMetrics",
+               paste0("R=", fastaFn),
+               paste0("I=", inBam),
+               paste0("O=", outputFn),
+               paste0("METRIC_ACCUMULATION_LEVEL=", metricLevel))
+  ezSystem(cmd)
+  
+  metrics <- ezRead.table(outputFn, comment.char="#", row.names=NULL)
+  metrics <- t(metrics)
+  return(metrics)
+}
