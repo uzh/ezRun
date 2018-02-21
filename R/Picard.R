d6566bc18949d0b7d47d2d252711837bde4e47a7
Ge Tan <gtan@me.com>
2018-02-21 10:34:39 +0100

    add CollectAlignmentSummaryMetrics man and fix output

--- R/Picard.R
+++ R/Picard.R
@@ -8,6 +8,7 @@
 CollectAlignmentSummaryMetrics <- function(inBam, fastaFn,
                                            metricLevel=c("ALL_READS", "SAMPLE",
                                                          "LIBRARY", "READ_GROUP")){
+  require(matrixStats)
   metricLevel <- match.arg(metricLevel)
   setEnvironments("picard")
   outputFn <- tempfile(pattern="CollectAlignmentSummaryMetrics",
@@ -17,11 +18,15 @@ CollectAlignmentSummaryMetrics <- function(inBam, fastaFn,
                paste0("R=", fastaFn),
                paste0("I=", inBam),
                paste0("O=", outputFn),
+               "METRIC_ACCUMULATION_LEVEL=null", ## clear the default "ALL_READS"
                paste0("METRIC_ACCUMULATION_LEVEL=", metricLevel))
   ezSystem(cmd)
   
   metrics <- ezRead.table(outputFn, comment.char="#", row.names=NULL)
+  metrics <- metrics[ ,!colAlls(is.na(metrics))] ## Remove the NA columns of nameColumns
   nameColumns <- c("SAMPLE", "LIBRARY", "READ_GROUP")
-  metrics <- metrics[ , c(nameColumns, setdiff(colnames(metrics), nameColumns))]
+  indexName <- intersect(colnames(metrics), nameColumns)
+  rownames(metrics) <- metrics[ ,indexName]
+  metrics[[indexName]] <- NULL
   return(metrics)
 }
