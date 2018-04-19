f4923678069a6f9967ccd3067930bc57b4efb406
Lennart Opitz <lopitz@fgcz.ethz.ch>
2018-04-19 10:53:45 +0200

    fix meme app and add filter options for peak sequences

--- R/app-meme.R
+++ R/app-meme.R
@@ -10,9 +10,42 @@ ezMethodMEME = function(input=NA, output=NA, param=NA){
   opt = param$cmdOptions
   db = param$motifDB
   sampleName = input$getNames()
-  cmd = paste(MEMECHIP,"-oc",sampleName,"-index-name",paste0(sampleName,"_meme-chip.html"),"-time 300 -order 1",db, opt, 
+  if(param$filterPeaks){
+    param$relPos = c('upstream','overlapStart')
+    peaks = ezRead.table(input$getFullPaths("CalledPeaks"), row.names = NULL)
+    peaks = peaks[abs(peaks$distancetoFeature) <=  param$distance, ]
+    peaks = peaks[peaks$insideFeature %in%  param$relPos,]
+    peaks = peaks[abs(peaks$fold_enrichment) >=  param$minFold, ]
+    toRemove = which(duplicated(peaks$name))
+    
+    if (length(toRemove)>0){
+      peaks = peaks[-toRemove,] }
+    ezWrite.table(peaks, basename(input$getFullPaths("CalledPeaks")),  row.names = F)
+    
+    #CreateBedFile:
+    bed = ezRead.table(input$getFullPaths("BED"), row.names = NULL, header = F)
+    bed = bed[bed$V4 %in% peaks$name,]
+    bed = bed[order(bed$V4),]
+    peaks = peaks[order(peaks$name),]
+    bed$V4 = paste0(bed$V4,'__',paste(peaks$chr,peaks$start,peaks$end,peaks$gene_name,peaks$distancetoFeature,sep='_'))
+    bedFileName = basename(input$getFullPaths("BED"))
+    ezWrite.table(bed, bedFileName, row.names = F, col.names = F)
+    
+    refFile = param$ezRef["refFastaFile"]
+    peakSeqFile = paste0(bedFileName, "_peaks.fa")
+    cmd = paste("/usr/local/ngseq/bin/bedtools", " getfasta -fi", refFile,
+                  " -bed ", bedFileName, " -name -fo ", peakSeqFile)
+    system(cmd)
+    cmd = paste("meme-chip -oc",sampleName,"-time 300 -order 1", db, opt, peakSeqFile)
+  } else{
+    cmd = paste("meme-chip -oc",sampleName,"-time 300 -order 1", db, opt, 
               input$getFullPaths("PeakSequences"))
+  }
+  
   ezSystem(cmd)
+  setwd(sampleName)
+  ezSystem(paste('mv meme-chip.html ',paste0(sampleName,"_meme-chip.html")))
+  setwd('..')
   return("Success")
 }
 
