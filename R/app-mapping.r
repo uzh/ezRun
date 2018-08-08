fbe8a19b71b1848d0bf5956dcb86b1aa415ebf58
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2018-08-08 12:15:56 +0200

    fix read groups

--- R/app-mapping.r
+++ R/app-mapping.r
@@ -310,7 +310,8 @@ ezMethodSTAR = function(input=NA, output=NA, param=NA){
               if(param$paired) trimmedInput$getColumn("Read2"),
               "--twopassMode", ifelse(param$twopassMode, "Basic", "None"),
               "--runThreadN", ezThreads(), param$cmdOptions, 
-              "--outStd BAM_Unsorted --outSAMtype BAM Unsorted", "--outSAMattrRGline", paste0("ID:", trimmedInput$getNames()),
+              "--outStd BAM_Unsorted --outSAMtype BAM Unsorted",
+              "--outSAMattrRGline", paste0("ID:", trimmedInput$getNames()), paste0("SM:", trimmedInput$getNames()),
               ">  Aligned.out.bam")## writes the output file Aligned.out.bam
   ##"|", "samtools", "view -S -b -", " >", "Aligned.out.bam")
   ezSystem(cmd)
