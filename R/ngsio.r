4b7f50e2dde1de4f50913926426b9e0a39b78875
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2018-08-10 15:53:54 +0200

    added some space

--- R/ngsio.r
+++ R/ngsio.r
@@ -67,6 +67,7 @@ loadCountDataset <- function(input, param){
     sigThresh = 0
   }
   
+  
   if (ezIsSpecified(param$correctBias) && param$correctBias){
     ## output will be floating point, but we don't round; input might already be floating point
     signal = ezCorrectBias(signal, gc=seqAnno$gc,
