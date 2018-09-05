fef1a6957412dba6b1a8e02cc1e7379b05b8ced1
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2018-09-05 15:32:43 +0200

    ibid.

--- R/app-edgerTwoGroups.R
+++ R/app-edgerTwoGroups.R
@@ -17,8 +17,7 @@ ezMethodEdger = function(input=NA, output=NA, param=NA,
   input = cleanupTwoGroupsInput(input, param)
   param$grouping = input$getColumn(param$grouping)
   if (ezIsSpecified(param$grouping2) && length(param$grouping2) == 1){
-    rawDataTemp = loadCountDataset(input, param)
-    grouping2Header <- grep(param$grouping2, names(colData(rawDataTemp)), value = T)
+    grouping2Header <- grep(param$grouping2, names(input$meta), value = T)
     param$grouping2 = input$getColumn(param$grouping2)
   }
   
