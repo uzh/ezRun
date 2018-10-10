1bdf08a927e5a0c7841a9b0bd94c5235ac757a0f
Ge Tan <gtan@me.com>
2018-10-10 15:28:46 +0200

    clean code

--- R/go-reports.R
+++ R/go-reports.R
@@ -58,46 +58,6 @@ goClusterTableRmd = function(param, clusterResult, seqAnno){
   return(list(ktables=ktables, linkTable=linkTable, enrichrTable=enrichrTable))
 }
 
-##' @title Adds the GO up-down results
-##' @description Adds the GO up-down results to an html file.
-##' @template doc-template
-##' @templateVar object result
-##' @param param a list of parameters to pass to \code{goUpDownTables()} and extract \code{doZip} from.
-##' @param goResult the GO result to get the up-down results from. Can be obtained by \code{twoGroupsGO()}.
-##' @seealso \code{\link{twoGroupsGO}}
-##' @template roxygen-template
-addGoUpDownResult = function(doc, param, goResult){
-  udt = goUpDownTables(param, goResult)
-  addParagraph(doc, paste("Maximum number of terms displayed:", param$maxNumberGroupsDisplayed))
-  
-  addFlexTable(doc, ezFlexTable(udt$linkTable, add.rownames=TRUE))
-  addTitle(doc, "GO categories that are overrepresented among significantly upregulated genes.", 3)
-  addFlexTable(doc, udt$flexTables[["enrichUp"]])
-  addTitle(doc, "GO categories that are overrepresented among significantly downregulated genes.", 3)
-  addFlexTable(doc, udt$flexTables[["enrichDown"]])
-  addTitle(doc, "GO categories that are overrepresented among all significantly regulated genes.", 3)
-  addFlexTable(doc, udt$flexTables[["enrichBoth"]])
-  
-  revigoLinks = ezMatrix("", rows=c('enrichBoth', 'enrichDown', 'enrichUp'), cols=c('BP', 'CC', 'MF'))
-  for (col in colnames(revigoLinks)){
-    for (row in rownames(revigoLinks)){
-      goSubResult = goResult[[col]][[row]]
-      if (all(is.na(goSubResult))) next
-      goSubResult = goSubResult[which(goSubResult$Pvalue < param$pValThreshFisher),]
-      if(nrow(goSubResult) > param$maxNumberGroupsDisplayed) {
-        goSubResult = goSubResult[1:param$maxNumberGroupsDisplayed,]
-      }
-      revigoLinks[row, col] = paste0('http://revigo.irb.hr/?inputGoList=',
-                                       paste(rownames(goSubResult), goSubResult[,'Pvalue'], collapse='%0D%0A'))
-      revigoLinks[row, col] = as.html(pot("ReViGO", hyperlink = revigoLinks[row, col]))
-    }
-  }
-  revigoTitle = "ReViGO"
-  addTitle(doc, revigoTitle, 3, id=revigoTitle)
-  addFlexTable(doc, ezFlexTable(revigoLinks, valign="middle", header.columns = TRUE, add.rownames = TRUE))
-  addTxtLinksToReport(doc, udt$txtFiles, param$doZip)
-  return(revigoTitle)
-}
 
 revigoUpDownTables <- function(param, goResult){
   revigoLinks = ezMatrix("", rows=c('enrichUp', 'enrichDown', 'enrichBoth'), 
