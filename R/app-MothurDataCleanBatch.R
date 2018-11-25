4ba0bf5d9a96a45148d95f0bf3f75db86ae2fd53
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2018-11-25 16:16:44 +0100

    fixed read ID parsing

--- R/app-MothurDataCleanBatch.R
+++ R/app-MothurDataCleanBatch.R
@@ -34,8 +34,8 @@ ezMethodMothurDataCleanBatch = function(input=NA, output=NA, param=NA,
     contigString = "###make"
     singleReadFileName <- paste0(sampleName,".R1")
     fastqFileToRead <- readDNAStringSet(paste0(singleReadFileName,".fastq"),format = "fastq")
-    groupFile <- data.frame(paste(names(fastqFileToRead), collapse = "_"),
-                                  singleReadFileName, stringsAsFactors = F)
+    readNames <- ldply(strsplit(names(fastqFileToRead)," "), function(x)x[1])$V1
+    groupFile <- data.frame(readNames, singleReadFileName, stringsAsFactors = F)
     groupFileName <- paste0(sampleName,".R1.groups")
     write.table(groupFile,groupFileName, col.names = F, row.names = F, quote = F)
     fastaOutName <- paste0(singleReadFileName,".fasta")
