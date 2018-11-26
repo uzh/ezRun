fca6b8a568e478c9dff1d78841bf94a50a88fd4f
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2018-11-26 10:21:26 +0100

    added Uparse and fixing Mothur

---
+++ R/app-Uparse.R
@@ -0,0 +1,122 @@
+###################################################################
+# Functional Genomics Center Zurich
+# This code is distributed under the terms of the GNU General
+# Public License Version 3, June 2007.
+# The terms are available here: http://www.gnu.org/licenses/gpl.html
+# www.fgcz.ch
+
+
+ezMethodUparse = function(input=NA, output=NA, param=NA, 
+                                                 htmlFile="00index.html"){
+### run UPARSE and reformat output for phylotseq 
+  
+library(plyr)
+library(dplyr)
+
+  sampleName = input$getNames()
+  file1PathInDatset <- input$getFullPaths("Read1")
+  cpCmd1 <- paste0("gunzip -c ", file2PathInDatset, "  > ", sampleName,"_R1",".fastq")
+  ezSystem(cpCmd1)  
+  if(param$paired){
+    file2PathInDatset <- input$getFullPaths("Read2")
+    cpCmd2 <- paste0("gunzip -c ", file2PathInDatset, "  > ", sampleName,"_R2",".fastq")
+    ezSystem(cpCmd2)
+    }
+uparseTemplScript <- USEARCH_TEMPLATE_SCRIPT
+uparseToBeExec <- paste0("uparse.",sampleName,".sh")
+
+updateTemplateScript <- paste0("sed -e s/\"SAMPLE_NAME\"/", sampleName, "/g",
+                               "sed -e s/\"MAX_EE\"/", param$fastqErrorMax, "/g ",
+                               uparseTemplScript, " >",
+                               uparseToBeExec)
+uparseCmd <- paste("bash",uparseToBeExec)
+
+### 1 OTU abundance table 
+inputTabFile <- paste0(sampleName,".OTU.tab.txt")
+otuTabRaw <- read.delim(inputTabFile, header = T, stringsAsFactors = F)
+colnames(otuTabRaw)[1] = "OTUid"
+otuTabRaw <- data.frame(t(otuTabRaw), stringsAsFactors = F)
+colnames(otuTabRaw) <- otuTabRaw[1,]
+otuTabRaw <- otuTabRaw[-1,]
+namesForRows <- rownames(otuTabRaw)
+nOTUS <- ncol(otuTabRaw)
+otuTabTransf <- data.frame(apply(otuTabRaw,2,as.numeric))
+rownames(otuTabTransf) <- namesForRows
+otuTabTransf["sample"] <- factor(ldply(strsplit(rownames(otuTabTransf),"_"),function(x) x[[1]])$V1)
+colsToKeep <- grep("Zotu",colnames(otuTabTransf), value = T)
+phyloseqAppFormatOtuAbundTable <- aggregate(otuTabTransf[,colsToKeep],by=list(otuTabTransf$sample),sum)
+names(phyloseqAppFormatOtuAbundTable)[1] <- "Group"
+phyloseqAppFormatOtuAbundTable$label <- "0.03"
+phyloseqAppFormatOtuAbundTable$numOtus <- nOTUS
+
+### 2 OTU-taxonomy table 
+inputTaxFile <- paste0(sampleName,".OTU.taxonomy.txt")
+otuTaxonomyRaw <- read.delim(inputTaxFile, header = F, stringsAsFactors = F)
+otuTaxonomyRaw <- otuTaxonomyRaw[,1:2]
+names(otuTaxonomyRaw) <- c("OTU","Taxonomy")
+bactRowsToKeep <- grep("d:Bacteria",otuTaxonomyRaw$Taxonomy)
+otuTaxonomyRawOnlyBact <- otuTaxonomyRaw[bactRowsToKeep,]
+taxRanks <- c("d:","p:","c:","o:","f:","g:","s:")
+createAppropriateRows <- function(inputTaxTable){
+  x <- inputTaxTable[2]
+  x1 <- strsplit(x,",")[[1]]
+  taxClassif <- vector()
+  taxClassif[1] <- "Bacteria"
+  for (k in 2:7){
+    tax <- taxRanks[k]
+    isDomThere <- grep(tax,x1, value = T)
+    if (length(isDomThere) >0){
+      taxClassif[k] <- unlist(strsplit(gsub(tax,"",isDomThere),"\\("))[1]
+    } else {
+      k1=k-1
+      alreadyUnclassified <- grep(taxClassif[k1],"unclassified")
+      if (length(alreadyUnclassified) > 0 ) {
+        taxClassif[k] <- taxClassif[k1]
+      } else{
+      taxClassif[k] <- paste(taxClassif[k1],"unclassified",sep = "_")
+    }
+    }
+  }
+ return(c(inputTaxTable[1],paste(taxClassif, collapse = ";")))
+}
+
+### compact everything 
+formattedRows <- apply(otuTaxonomyRawOnlyBact,1,createAppropriateRows)
+formattedDF <- data.frame(t(listOfFormattedRows), stringsAsFactors = F)
+colnames(formattedDF) <- c("OTU","Taxonomy")
+OTUsToKeep <- colnames(phyloseqAppFormatOtuAbundTable)[
+  colnames(phyloseqAppFormatOtuAbundTable)%in%formattedDF$OTU]
+finalOtuAbundTable <- phyloseqAppFormatOtuAbundTable[,c("Group", "label","numOtus",OTUsToKeep)]
+finalOtuTaxon <- formattedDF[formattedDF$OTU%in%OTUsToKeep,]
+OTUsTotAbund <- apply(finalOtuAbundTable,2,sum)
+finalOtuTaxon$Size <- OTUsTotAbund
+
+## sort final files 
+newOtuAbundFile <- basename(output$getColumn("OtuAbundFile"))
+newTaxAbundFile <- basename(output$getColumn("TaxAbundFile"))
+write.table(finalOtuAbundTable,newOtuAbundFile,row.names = F, col.names = T, quote = F)
+write.table(finalOtuTaxon,newTaxAbundFile,row.names = F, col.names = T, quote = F)
+}
+
+##' @template app-template
+##' @templateVar method ezMethodUparse()
+##' @templateVar htmlArg )
+##' @description Use this reference class to run 
+EzAppUparse <-
+  setRefClass("EzAppUparse",
+              contains = "EzApp",
+              methods = list(
+                initialize = function()
+                {
+                  "Initializes the application using its specific defaults."
+                  runMethod <<- ezMethodUparse
+                  name <<- "EzAppUparse"
+                  appDefaults <<- rbind(fastqErrorMax = ezFrame(Type="numeric",
+                                                                 DefaultValue="1",
+                                                                 Description="Max EE in fastx_truncate")
+
+                  )
+                }
+              )
+  )
+
