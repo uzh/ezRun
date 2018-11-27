c85cd9604b4d2f1424ecf1223fb904397c5d23d6
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2018-11-27 12:44:36 +0100

    mothur workflow output updates and added report

---
+++ R/app-MothurDataAnalysisReport.R
@@ -0,0 +1,62 @@
+###################################################################
+# Functional Genomics Center Zurich
+# This code is distributed under the terms of the GNU General
+# Public License Version 3, June 2007.
+# The terms are available here: http://www.gnu.org/licenses/gpl.html
+# www.fgcz.ch
+
+
+ezMethodMothurDataAnalysisReport = function(input=NA, output=NA, param=NA, 
+                                   htmlFile="00index.html"){
+  
+  require(rmarkdown)
+  require(ShortRead)
+  require(phyloseq)
+  require(plyr)
+  require(ape)
+  require(ggplot2)
+  library(scales)
+  require(gridExtra)
+  dataset = input$meta
+
+  
+  ## Define input for rmd file
+  RawDataSummary <- input$getFullPaths("RawDataSummary")
+  DeduppedSummary <- input$getFullPaths("DeduppedSummary")
+  LenAndHomopSummary <- input$getFullPaths("LenAndHomopSummary")
+  MapFiltSummary <- input$getFullPaths("MapFiltSummary")
+  ChimeraPlot <- input$getFullPaths("ChimeraPlot")
+  PreClusteredAndChimeraSummary <- input$getFullPaths("PreClusteredAndChimeraSummary")
+  stepConvergence <- input$getFullPaths("stepConvergence")
+  
+  
+  ## Copy the style files and templates
+  styleFiles <- file.path(system.file("templates", package="ezRun"),
+                          c("fgcz.css", "MothurDataAnalysisReport.Rmd",
+                            "fgcz_header.html", "banner.png"))
+  file.copy(from=styleFiles, to=".", overwrite=TRUE)
+  rmarkdown::render(input="MothurDataAnalysisReport.Rmd", envir = new.env(),
+                    output_dir=".", output_file=htmlFile, quiet=TRUE)
+}
+
+##' @template app-template
+##' @templateVar method ezMethodMothurDataAnalysisReport()
+##' @templateVar htmlArg )
+##' @description Use this reference class to run 
+EzAppMothurDataAnalysisReport <-
+  setRefClass("EzAppMothurDataAnalysisReport",
+              contains = "EzApp",
+              methods = list(
+                initialize = function()
+                {
+                  "Initializes the application using its specific defaults."
+                  runMethod <<- ezMethodMothurDataAnalysisReport
+                  name <<- "EzAppMothurDataAnalysisReport"
+                  appDefaults <<- rbind(RepresentativeOTUs = ezFrame(Type="integer",
+                                                         DefaultValue="80",
+                                                         Description="Number of representative OTUs")
+                                   
+                  )
+                }
+              )
+  )
