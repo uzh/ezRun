bbbe49cdf7a5440faee7304b6ff2838de7977090
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2018-12-03 12:39:30 +0100

    tset

--- R/mothurPhyloseqAssociatedFunctions.R
+++ R/mothurPhyloseqAssociatedFunctions.R
@@ -294,8 +294,8 @@ heatmapForPhylotseqPlot <- function(phyloseqOtuObj){
   cols <- colorRampPalette(brewer.pal(10, "RdBu"))(256)
   ## heatmap
     heatmap.2(z$data,dendrogram=c("both"),Rowv=z$Rowv,Colv=z$Colv,col=rev(cols), 
-              trace='none',density.info=c("none"), 
-              labRow=NA)
+              trace='none',density.info=c("none"),keysize = 0.8, 
+              labRow=NA,cexCol = 1)
   }
 }
 
