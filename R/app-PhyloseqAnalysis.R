3d09dc612039273ee66a197c88893063b77d7448
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2018-12-06 22:34:41 +0100

    group

--- R/app-PhyloseqAnalysis.R
+++ R/app-PhyloseqAnalysis.R
@@ -23,7 +23,7 @@ ezMethodPhyloSeqAnalysis = function(input=NA, output=NA, param=NA,
   library(pheatmap)
   
   dataset = input$meta
-  isGroupThere = param$group
+  isGroupThere = nlevels(input$getColum("Group")) > 1
 ### Analyzes results with phyloseq: preparing objects to be processed in the Rmd file
 
 ### OTUs
