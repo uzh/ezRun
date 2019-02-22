3fe945c00c0982589c4ac2ff491d4ac5852d7ae0
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2019-02-22 14:44:28 +0100

    typo

--- R/app-DADA2Step1Sample.R
+++ R/app-DADA2Step1Sample.R
@@ -13,15 +13,16 @@ ezMethodDADA2Step1Sample = function(input=NA, output=NA, param=NA,
   require(purrr)
   dataset = input$meta
   sampleName = input$getNames() 
+  minLen <- param$minLen
+  maxLen <- param$maxLen
+  isPaired <- param$paired
   ### read fastq files and prepare inputs for DADA2
   ### are reads paired? should they be joined? 
   file1PathInDataset <- input$getFullPaths("Read1")
-  if(param$paired){
+  if(isPaired){
     file2PathInDataset <- input$getFullPaths("Read2")
   }
-  minLen <- param$minLen
-  maxLen <- param$maxLen
-  DADA2mainSeqTabObj <- DADA2CreateSeqTab(sampleName,maxLen,minLen,param$paired)
+  DADA2mainSeqTabObj <- DADA2CreateSeqTab(sampleName,maxLen,minLen,isPaired)
   
   ## rename output files
   ## Files needed for the report 
