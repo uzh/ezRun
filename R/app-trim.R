d3f309f1d9dc462737a8bc47cdb9ab8e7b9531a3
Lennart Opitz <lopitz@fgcz.ethz.ch>
2019-04-25 11:03:09 +0200

    fix bug when calling trimmomatic

--- R/app-trim.R
+++ R/app-trim.R
@@ -162,7 +162,7 @@ ezMethodTrim = function(input=NA, output=NA, param=NA){
       readOpts = paste(
         input$getFullPaths("Read1"), r1TmpFile)
     }
-    cmd = paste(prepareTrimmomatic(), "-Djava.io.tmpdir=.", method,
+    cmd = paste(prepareTrimmomatic(), method,
                 ## hardcode phred33 quality encoding
                 "-threads", min(param$cores, 8), "-phred33",
                 readOpts, trimAdaptOpt, tailQualOpt, leadingQualOpt, trailingQualOpt, minAvgQualOpt,
