fd2f4cbc0a10e85c7c5162b6d57300e295418ea9
Lennart Opitz <lopitz@fgcz.ethz.ch>
2019-06-04 13:43:34 +0200

    deactivate strandnessCheck because it fails for sushi demo

--- R/app-mapping.r
+++ R/app-mapping.r
@@ -464,7 +464,7 @@ EzAppSTAR <-
                   appDefaults <<- rbind(getJunctions=ezFrame(Type="logical",  DefaultValue="FALSE",	Description="should junctions be returned"),
                                         writeIgvSessionLink=ezFrame(Type="logical", DefaultValue="TRUE", Description="should an IGV link be generated"),
                                         markDuplicates=ezFrame(Type="logical", DefaultValue="TRUE", Description="should duplicates be marked with picard"),
-                                        checkStrandness=ezFrame(Type="logical", DefaultValue="TRUE", Description="should strandness be checked"),
+                                        checkStrandness=ezFrame(Type="logical", DefaultValue="FALSE", Description="should strandness be checked"),
                                         randomSleep=ezFrame(Type="logical",  DefaultValue="FALSE",  Description="should there be a random sleep to avoid to much network traffic when loading the STAR index"),
                                         twopassMode=ezFrame(Type="logical", DefaultValue="TRUE", Description="1-pass mapping or basic 2-pass mapping")
                                         )
