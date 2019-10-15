f3aa38c284548ea6016f9a00bcc13d33284f886d
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2019-10-15 23:56:52 +0200

    fixed rownames dseign matric

--- R/app-DADA2Step1Sample.R
+++ R/app-DADA2Step1Sample.R
@@ -75,6 +75,7 @@ ezMethodDADA2Step1Sample = function(input=NA, output=NA, param=NA,
     factorCols <- grep("Factor",colnames(dataset))
     designMatrix <- data.frame(dataset[,factorCols])
     colnames(designMatrix) <- gsub(" \\[Factor\\]","",colnames(dataset)[factorCols])
+    rownames(designMatrix) <- rownames(dataset)
     designMatrixFile <-  basename(output$getColumn("sampleDescriptionFile"))
     write.table(designMatrix,designMatrixFile,row.names = F, col.names = T, quote = F,sep = "\t")
   }
