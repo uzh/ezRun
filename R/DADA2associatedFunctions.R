ac40f31c873862c343574617e352560b94a35fbc
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2019-10-15 22:27:49 +0200

    removed dada2 merging; using fastqjoin instead

--- R/DADA2associatedFunctions.R
+++ R/DADA2associatedFunctions.R
@@ -12,32 +12,7 @@
 ##' @param  a fastq or pairs of ffastq files .
 ##' @return Returns a DADA2 seqtab object.
 
-DADA2CreateSeqTab <- function(sampleNames,minLen=0,concat=NULL,file1PathInDataset,
-                              file2PathInDataset=NULL,database){
-  if(!missing(file2PathInDataset)){
-  fnFs <- file1PathInDataset
-  fnRs <- file2PathInDataset
-  filtFs <- "filt.R1.fastq.gz"
-  filtRs <- "filt.R2.fastq.gz"
-  
-  out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(minLen,minLen),
-                       maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
-                       compress=TRUE, multithread=TRUE)
-  errF <- learnErrors(filtFs, errorEstimationFunction = noqualErrfun, multithread=TRUE)
-  errR <- learnErrors(filtRs, errorEstimationFunction = noqualErrfun, multithread=TRUE)
-  derepFs <- derepFastq(filtFs, verbose=TRUE)
-  derepRs <- derepFastq(filtRs, verbose=TRUE)
-  
-  dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
-  dadaRs <- dada(derepRs, err=errR, multithread=TRUE)
-  
-  mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, verbose=TRUE, 
-                        justConcatenate = concat)
-  seqtab <- makeSequenceTable(mergers)
-  fullTableOfOTUsNoChim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE)
-  taxa <- assignTaxonomy(fullTableOfOTUsNoChim,database, multithread=TRUE)
-  rownames(fullTableOfOTUsNoChim) <- sampleNames
-  }else{
+DADA2CreateSeqTab <- function(sampleNames,minLen=0,file1PathInDataset,database){
     fnFs <- file1PathInDataset
     filtFs <- paste0(sampleNames,".filt.R1.fastq.gz")
     names(filtFs) <- sampleNames
@@ -45,13 +20,12 @@ DADA2CreateSeqTab <- function(sampleNames,minLen=0,concat=NULL,file1PathInDatase
                          maxN=0, maxEE=1, truncQ=11, rm.phix=TRUE,
                          compress=TRUE, multithread=TRUE)
     errF <- learnErrors(filtFs,errorEstimationFunction = noqualErrfun, multithread=TRUE)
-    derepFs <- derepFastq(filtFs, verbose=TRUE)
+    derepFs <- derepFastq(filtFs, verbose=FALSE)
     dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
     seqtab <- makeSequenceTable(dadaFs)
     fullTableOfOTUsNoChim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE)
     taxa <- assignTaxonomy(fullTableOfOTUsNoChim,database, multithread=TRUE)
     rownames(fullTableOfOTUsNoChim) <- sampleNames
-  }
   return(list(fullTableOfOTUsNoChimObj=fullTableOfOTUsNoChim,taxaObj=taxa))
 }
 
