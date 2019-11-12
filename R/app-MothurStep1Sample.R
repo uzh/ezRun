d44216e9cbf89e2aec27752b2b88950c0e42f521
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2019-11-12 21:02:18 +0100

    main commit consolidating mothur apps

--- R/app-MothurStep1Sample.R
+++ R/app-MothurStep1Sample.R
@@ -17,110 +17,117 @@ ezMethodMothurStep1Sample = function(input=NA, output=NA, param=NA,
   require(ggplot2)
   library(scales)
   dataset = input$meta
-  sampleName = input$getNames() 
+  sampleNames = input$getNames() 
+  isPaired <- param$paired
   ### read fastq files and prepare inputs for Mothur
-  ### are reads paired? should they be joined? 
-  file1PathInDatset <- input$getFullPaths("Read1")
-  unzipped1 <- paste0(sampleName,".R1.fastq")
-  cpCmd <- paste0("gunzip -c ", file1PathInDatset, "  > ", unzipped1)
-  ezSystem(cpCmd)
-  if(param$paired){
-    contigString = "make" 
-    file2PathInDatset <- input$getFullPaths("Read2")
-    unzipped2 <- paste0(sampleName,".R2.fastq")
-    cpCmd2 <- paste0("gunzip -c ", file2PathInDatset, "  > ", unzipped2)
-    ezSystem(cpCmd2)
-    filesNameMothur <- cbind(input$getNames(),unzipped1,unzipped2)
-    filesNameMothurFile <- paste(sampleName,"files",sep = ".")
-    write.table(filesNameMothur,filesNameMothurFile,row.names = F,col.names = F, quote = F)
-    initialFastaSuffix = "trim.contigs.fasta"
-    initialGroupSuffix = "contigs.groups"
-  }else{
-    contigString = "###make"
-    singleReadFileName <- paste0(sampleName,".R1")
-    fastqFileToRead <- readDNAStringSet(paste0(singleReadFileName,".fastq"),format = "fastq")
+  ### File preparation: are reads paired? should they be joined? 
+  file1PathInDataset <- input$getFullPaths("Read1")
+  if(isPaired){
+    file2PathInDataset <- input$getFullPaths("Read2")
+    fastqJoin="/usr/local/ngseq/src/ea-utils.1.1.2-686/fastq-join"
+    
+    fastqJoinFun <- function(x,y,z){
+      joinedFileName <- paste0(z, ".temp.")
+      fastqJoinCmd <- paste(fastqJoin, x,y, "-o", joinedFileName)
+      ezSystem(fastqJoinCmd)
+      joinedFileName <- paste0(joinedFileName,"join")
+      joinedFile <- file.path(getwd(),joinedFileName)
+      return(joinedFile)
+    }
+    listOfJoinedFiles <- mapply(fastqJoinFun,file1PathInDataset,file2PathInDataset,
+                                sampleNames)
+    }else{
+      listOfJoinedFiles <- vector()
+      i=0
+      for (singleEndFile in file1PathInDataset){
+      i=i+1
+      listOfJoinedFiles[i] <-  paste0(sampleNames[i],".fastq")
+    cpCmd <- paste0("gunzip -c ", singleEndFile, "  > ", listOfJoinedFiles[i])
+    ezSystem(cpCmd)
+      }
+    }
+  
+  ### create mothur input fasta and group files  
+    for (sampleName in sampleNames){
+    fastqFileToRead <- readDNAStringSet(paste0(sampleName,".fastq"),format = "fastq")
     readNames <- ldply(strsplit(names(fastqFileToRead)," "), function(x)x[1])$V1
     groupFile <- data.frame(readNames, sampleName, stringsAsFactors = F)
-    groupFileName <- paste0(sampleName,".R1.groups")
-    write.table(groupFile,groupFileName, col.names = F, row.names = F, quote = F)
-    fastaOutName <- paste0(singleReadFileName,".fasta")
-    fastaFileToWrite <- writeXStringSet(fastqFileToRead, fastaOutName)
-    initialFastaSuffix = "R1.fasta"
-    initialGroupSuffix = "R1.groups"
-  }
-  
-  ### is there at least a mock sample for the error estimate? The error estimates for the Non-mock samples will be ignored downstream
-  if(param$mockSample){
-    if (input$getColumn("Mock") == "Yes") {
-      copyRefCmd <- paste("cp", param$referenceFasta,"./", sep = " ")
-      ezSystem(copyRefCmd)
-      mockString = "seq.error" 
-      refString = param$referenceFasta
-      oldErrFile <- paste(sampleName,
-                          "unique.good.good.good.filter.unique.precluster.pick.pick.error.count",
-                          sep = ".")
-    }else{
-      mockString = "###seq.error"  
-      oldErrFile <- "mockErrorFileNotForDownstreamAnalysis.txt"
-      write("There is no relevant info in this file",oldErrFile)
+    groupFileName <- "Mothur.groups"
+    write.table(groupFile,groupFileName, col.names = F, row.names = F, quote = F,append = TRUE)
+    fastaOutName <- "Mothur.fasta"
+    fastaFileToWrite <- writeXStringSet(fastqFileToRead, fastaOutName,append=TRUE)
     }
-  }else{
-    mockString = "###seq.error"
-  }
-  ###
+  
+  # ### TODO: is there at least a mock sample for the error estimate? The error estimates for the Non-mock samples will be ignored downstream
+  # if(param$mockSample){
+  #   if (input$getColumn("Mock") == "Yes") {
+  #     copyRefCmd <- paste("cp", param$referenceFasta,"./", sep = " ")
+  #     ezSystem(copyRefCmd)
+  #     mockString = "seq.error" 
+  #     refString = param$referenceFasta
+  #     oldErrFile <- paste(sampleName,
+  #                         "unique.good.good.good.filter.unique.precluster.pick.pick.error.count",
+  #                         sep = ".")
+  #   }else{
+  #     mockString = "###seq.error"  
+  #     oldErrFile <- "mockErrorFileNotForDownstreamAnalysis.txt"
+  #     write("There is no relevant info in this file",oldErrFile)
+  #   }
+  # }else{
+  #   mockString = "###seq.error"
+  # }
+  # ###
 
   ### cp silva reference locally
-  cpSilvaRefCmd <- paste("cp ",SILVA_DB_MOTHUR, "silva.bacteria.fasta")
+  cpSilvaRefCmd <- paste("gunzip -c ",SILVA_DB_MOTHUR, " > silva.bacteria.fasta")
   ezSystem(cpSilvaRefCmd)
   
   ### update batch file  with parameters and run mothur: step 1, identify region
   updateBatchCmd1 <- paste0("sed -e s/\"MIN_LEN\"/", param$minLen, "/g",
                            " -e s/\"MAX_LEN\"/", param$maxLen, "/g",
-                           " -e s/\"INITIAL_SUFFIX_FASTA\"/", initialFastaSuffix, "/g",
-                           " -e s/\"INITIAL_SUFFIX_GROUPS\"/", initialGroupSuffix, "/g",
-                           " -e s/\"Mothur\"/", sampleName,"/g",
-                           " -e s/\"###make\"/", contigString,"/g ",
-                           file.path(METAGENOMICS_ROOT,FINAL_MOTHUR_WORKFLOW_TEMPLATE_STEP1), 
-                           " >",
-                           FINAL_MOTHUR_WORKFLOW_TEMPLATE_STEP1)
+                           " -e s/\"CUTOFF_TAXON\"/", param$cutOffTaxonomy, "/g",
+                           " -e s/\"CUTOFF_CLUST\"/", param$cutOffCluster, "/g",
+                           " -e s/\"DIFFS\"/", param$diffs, "/g ",
+                           file.path(METAGENOMICS_ROOT,UNIFIED_MOTHUR_WORKFLOW), 
+                           " > ",
+                           UNIFIED_MOTHUR_WORKFLOW)
   ezSystem(updateBatchCmd1)
-  cmdMothur1 = paste(MOTHUR_EXE,FINAL_MOTHUR_WORKFLOW_TEMPLATE_STEP1)
+  cmdMothur1 = paste(MOTHUR_EXE,UNIFIED_MOTHUR_WORKFLOW)
   ezSystem(cmdMothur1)
- 
+  ## create phyloseq object
+  # OTU count
+  newOTUsToCountFileName <- basename(output$getColumn("OTUsCountTable"))
+  otuMothurFile <- "merged.good.filter.unique.precluster.pick.opti_mcc.shared"
+  countOTUs <- phyloSeqOTUFromFile(otuMothurFile)
+  write.table(countOTUs,newOTUsToCountFileName,
+              row.names = F, col.names = T, quote = F,sep = "\t")
+  # taxonomy
+  newOTUsToTaxFileName <- basename(output$getColumn("OTUsToTaxonomyFile"))
+  taxaMothurFile <- paste("merged.good.filter.unique.precluster.pick.opti_mcc",
+                          param$cutOffCluster,"cons.taxonomy",sep = ".")
+    taxaOTUs <- phyloSeqTaxaFromFile(taxaMothurFile)
+  write.table(taxaOTUs,newOTUsToTaxFileName,
+              row.names = F, col.names = T, quote = F,sep = "\t")
   
-  ## rename output files
-  ## Files needed for the report 
-  #1) 
-  oldRawSummary <- paste(sampleName,"summary",
-                         sep = ".")
-  newRawSummary <- basename(output$getColumn("RawDataSummary"))
-  ezSystem(paste("mv",oldRawSummary,newRawSummary))
+  ## design Matrix 
+  if (param$group){
+    factorCols <- grep("Factor",colnames(dataset))
+    designMatrix <- data.frame(dataset[,factorCols])
+    colnames(designMatrix) <- gsub(" \\[Factor\\]","",colnames(dataset)[factorCols])
+    rownames(designMatrix) <- rownames(dataset)
+    newdesignMatrixFileName <- basename(output$getColumn("OTUsDesignMatrix"))
+    write.table(designMatrix,newdesignMatrixFileName,
+                row.names = F, col.names = T, quote = F,sep = "\t")
+  }
   
-  #2) 
-  oldDeduppedFileName <- paste(sampleName,"unique.summary",
-                               sep = ".")
-  newDeduppedFileName <- basename(output$getColumn("DeduppedSummary"))
-  ezSystem(paste("mv",oldDeduppedFileName,newDeduppedFileName))
-  
-  #3) 
-  oldLengthAndHomopFilteredFileName <- paste(sampleName,"unique.good.summary",
-                                             sep = ".")
-  newLengthAndHomopFilteredFileName <- basename(output$getColumn("LenAndHomopSummary"))
-  ezSystem(paste("mv",oldLengthAndHomopFilteredFileName,newLengthAndHomopFilteredFileName))
-  
-
-  ## file needed for the second step 
-  #5) 
-  oldMappedFilteredFileName <- paste(sampleName,"unique.good.align",
-                                     sep = ".")
-  newMappedFilteredFileName <- basename(output$getColumn("alignedFile"))
-  ezSystem(paste("mv",oldMappedFilteredFileName,newMappedFilteredFileName))
-  
-  oldMappedFilteredFileName <- paste(sampleName,"groups",
-                                     sep = ".")
-  newMappedFilteredFileName <- basename(output$getColumn("groupFile"))
-  ezSystem(paste("mv",oldMappedFilteredFileName,newMappedFilteredFileName))
-
+  ## create phyloseqObject
+  phyloseqObjectRdata <-  basename(output$getColumn("RObjectPhyloseq"))
+  if (param$group){
+    phyloseqObject <- phyloseq(countOTUs,taxaOTUs,sample_data(designMatrix))
+  }else{
+    phyloseqObject <- phyloseq(countOTUs,taxaOTUs)
+  }
+  saveRDS(phyloseqObject,phyloseqObjectRdata)
 }
 
 ##' @template app-template
@@ -136,9 +143,11 @@ EzAppMothurStep1Sample <-
                   "Initializes the application using its specific defaults."
                   runMethod <<- ezMethodMothurStep1Sample
                   name <<- "EzAppMothurStep1Sample"
-                  appDefaults <<- rbind(minLen = ezFrame(Type="integer",  DefaultValue="290",Description="Min length"),     
-                                        maxLen= ezFrame(Type="integer",  DefaultValue="330",Description="Max length"),
-                                        referenceFasta = ezFrame(Type="character",  DefaultValue="",Description="Mock reference seqeuences.")
+                  appDefaults <<- rbind(minLen = ezFrame(Type="integer",  DefaultValue="150",Description="Min length"),     
+                                        maxLen= ezFrame(Type="integer",  DefaultValue="450",Description="Max length"),
+                                        cutOffTaxonomy= ezFrame(Type="integer",  DefaultValue="80",Description="cut-off taxonomy"),
+                                        cutOffCluster= ezFrame(Type="numeric",  DefaultValue="0.03",Description="cut-off cluster"),
+                                        diffs= ezFrame(Type="integer",  DefaultValue="0.03",Description="differences")
                   )
                 }
               )
