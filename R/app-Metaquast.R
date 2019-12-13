e4723db0a32b1c25fb1be04fab49c38ef405fc35
giancarloRussoFGCZ <giancarlo.russo@fgcz.ethz.ch>
2019-12-13 01:26:20 +0100

    typo

--- R/app-Metaquast.R
+++ R/app-Metaquast.R
@@ -20,7 +20,7 @@ ezMethodMetaquast = function(input=NA, output=NA, param=NA, htmlFile="00index.ht
     listOfRef <- readDNAStringSet(refFile)
     listOfRefNames <- vector()
     for (fileName in names(listOfRef)) {
-  #    writeXStringSet(refFile[[fileName]],paste0(fileName,".fasta"))
+      writeXStringSet(refFile[[fileName]],paste0(fileName,".fasta"))
       listOfRefNames[fileName] <- paste0(fileName,".fasta")
     }
     listOfRefToParse <- paste(listOfRefNames, collapse = ",")
