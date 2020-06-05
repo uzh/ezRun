0e669c55e54b1277498ec1bf18d5e3df7c94327d
Ge Tan <gtan@me.com>
2020-06-05 16:21:12 +0200

    exceRot: remove hard coded paths. Use modules.

--- R/app-exceRpt.R
+++ R/app-exceRpt.R
@@ -16,23 +16,9 @@ EzAppExceRpt =
                    "Initializes the application using its specific defaults."
                    runMethod <<- ezMethodExceRpt
                    name <<- "EzAppExceRpt"
-                   appDefaults <<- rbind(
-                     # define full paths for excerpt's makefile
-                     EXE_DIR = ezFrame(Type="character", DefaultValue='/usr/local/ngseq/packages/Tools/exceRpt/4.6.3', Description="Path to smallRNA makefile"),
-                     
-                     FASTX_CLIP_EXE=ezFrame(Type="character", DefaultValue='/usr/local/ngseq/bin/fastx_clipper', Description="Path to binary"),
-                     FASTX_FILTER_EXE=ezFrame(Type="character", DefaultValue='/usr/local/ngseq/bin/fastq_quality_filter', Description="Path to binary"),
-                     BOWTIE2_EXE=ezFrame(Type="character", DefaultValue='/usr/local/ngseq/packages/Aligner/Bowtie2/2.3.2/bin/bowtie2', Description="Path to binary"),
-                     SAMTOOLS_EXE=ezFrame(Type="character", DefaultValue='/usr/local/ngseq/packages/Tools/samtools/1.8/bin/samtools', Description="Path to binary"),
-                     JAVA_EXE=ezFrame(Type="character", DefaultValue='/usr/local/ngseq/packages/Dev/jdk/8/bin/java', Description="Path to binary"),
-                     FASTQC_EXE=ezFrame(Type="character", DefaultValue="'/usr/local/ngseq/packages/Dev/jdk/8/bin/java -classpath /usr/local/ngseq/packages/QC/FastQC/0.11.7:/usr/local/ngseq/packages/QC/FastQC/0.11.7/sam-1.103.jar:/usr/local/ngseq/packages/QC/FastQC/0.11.7/jbzip2-0.9.jar'", Description="Paths to java and fastqc binaries"),
-                     STAR_EXE=ezFrame(Type="character", DefaultValue='/usr/local/ngseq/packages/Aligner/STAR/2.5.2b/bin/STAR', Description="Path to binary")
-                     )
                  }
                )
-               
   )
-
 
 ##' @title execute exceRpt_smallRNA.
 ##' @description R wrapper to execute exceRpt_smallRNA pipeline to count smallRNAs in bulk RNA-seq experiments.
@@ -47,20 +33,23 @@ ezMethodExceRpt = function(input=NA, output=NA, param=NA){
   stopifnot(length(param[['ADAPTER_SEQ']]) == 1)
   
   outputDir = basename(output$getColumn("excerpt"))
-  if(!dir.exists(outputDir)){dir.create(outputDir)}
+  dir.create(outputDir)
   
   ## objects from sushi form
-  param[['SAMPLE_NAME']]= input$getNames()
-  param[['refBuild']] = gsub('.*/','',param[['refBuild']])
+  param[['SAMPLE_NAME']] <- input$getNames()
+  param[['refBuild']] = gsub('.*/', '', param[['refBuild']])
   
   ## create command to run exceRpt_smallRNA
   cmd = pasteExcerptCmd(readFile, outputDir, param)
-  if(ezIsSpecified(param[['cmdOptions']])) cmd = paste(cmd, param[['cmdOptions']])
+  if(ezIsSpecified(param[['cmdOptions']])) 
+    cmd = paste(cmd, param[['cmdOptions']])
   ezSystem(cmd)
   
   ## Function to delete all files in OUTPUT_DIR that are not in CORE_RESULTS; remove unnecessary
   ## data to create report.
-  if(param[['REMOVE_LARGE_INTERMEDIATE_FILES']]=='true'){keepOnlyCoreFiles(outputDir)}
+  if(param[['REMOVE_LARGE_INTERMEDIATE_FILES']]=='true'){
+    keepOnlyCoreFiles(outputDir)
+  }
 }
 
 
@@ -73,51 +62,50 @@ ezMethodExceRpt = function(input=NA, output=NA, param=NA){
 pasteExcerptCmd=function(readFile, outputDir, param){
   cmd = paste(
         ## makefile to execute
-        paste0('make -f ',param[['EXE_DIR']],'/exceRpt_smallRNA'),
-        paste0('MAX_RAM=',paste0(param[['ram']],'G')),                                                 
-        paste0('N_THREADS=',param[['cores']]),
+        paste0('make -f ', file.path(Sys.getenv("exceRpt"), 'exceRpt_smallRNA')),
+        paste0('MAX_RAM=', param[['ram']], 'G'),
+        paste0('N_THREADS=', param[['cores']]),
         
         ## required options
-        paste0('INPUT_FILE_PATH=',readFile),   
-        paste0('OUTPUT_DIR=',outputDir),                   
+        paste0('INPUT_FILE_PATH=',readFile),
+        paste0('OUTPUT_DIR=',outputDir),
         
-        paste0('EXE_DIR=',param[['EXE_DIR']]),
+        paste0('EXE_DIR=', Sys.getenv("exceRpt")),
         
         ## paths to our modules
-        paste0('FASTX_CLIP_EXE=',param[['FASTX_CLIP_EXE']]),
-        paste0('FASTX_FILTER_EXE=',param[['FASTX_FILTER_EXE']]),
-        paste0('BOWTIE2_EXE=',param[['BOWTIE2_EXE']]),
-        paste0('SAMTOOLS_EXE=',param[['SAMTOOLS_EXE']]),
-        paste0('JAVA_EXE=',param[['JAVA_EXE']]),
-        paste0('FASTQC_EXE=',param[['FASTQC_EXE']]),
-        paste0('STAR_EXE=',param[['STAR_EXE']]),
+        paste0('FASTX_CLIP_EXE=', Sys.which("fastx_clipper")),
+        paste0('FASTX_FILTER_EXE=', Sys.which("fastq_quality_filter")),
+        paste0('BOWTIE2_EXE=', Sys.which("bowtie2")),
+        paste0('SAMTOOLS_EXE=', Sys.which("samtools")),
+        paste0('JAVA_EXE=', Sys.which("java")),
+        paste0('FASTQC_EXE=', Sys.which("fastqc")),
+        paste0('STAR_EXE=', Sys.which("STAR")),
         
         ## main analysis options
-        paste0('ADAPTER_SEQ=',param[['ADAPTER_SEQ']]),                         
-        paste0('SAMPLE_NAME=',param[['SAMPLE_NAME']]),
-        paste0('MAIN_ORGANISM_GENOME_ID=',param[['refBuild']]),
-        
-        paste0('ENDOGENOUS_LIB_PRIORITY=',param[['ENDOGENOUS_LIB_PRIORITY']]),
+        paste0('ADAPTER_SEQ=', param[['ADAPTER_SEQ']]),
+        paste0('SAMPLE_NAME=', param[['SAMPLE_NAME']]),
+        paste0('MAIN_ORGANISM_GENOME_ID=', param[['refBuild']]),
+        paste0('ENDOGENOUS_LIB_PRIORITY=', param[['ENDOGENOUS_LIB_PRIORITY']]),
         
         ## additional analysis options
-        paste0('TRIM_N_BASES_5p=',param[['TRIM_N_BASES_5p']]),                    
+        paste0('TRIM_N_BASES_5p=',param[['TRIM_N_BASES_5p']]),
         paste0('TRIM_N_BASES_3p=',param[['TRIM_N_BASES_3p']]),
-        paste0('MIN_READ_LENGTH=',param[['MIN_READ_LENGTH']]),                                   
-        paste0('QFILTER_MIN_QUAL=',param[['QFILTER_MIN_QUAL']]),                                 
+        paste0('MIN_READ_LENGTH=',param[['MIN_READ_LENGTH']]),
+        paste0('QFILTER_MIN_QUAL=',param[['QFILTER_MIN_QUAL']]),
         paste0('QFILTER_MIN_READ_FRAC=',param[['QFILTER_MIN_READ_FRAC']]), 
         
-        paste0('RANDOM_BARCODE_LENGTH=',param[['RANDOM_BARCODE_LENGTH']]),         
-        paste0('RANDOM_BARCODE_LOCATION=',"'",param[['RANDOM_BARCODE_LOCATION']],"'"),     
-        paste0('KEEP_RANDOM_BARCODE_STATS=',param[['KEEP_RANDOM_BARCODE_STATS']]),
+        paste0('RANDOM_BARCODE_LENGTH=', param[['RANDOM_BARCODE_LENGTH']]),
+        paste0('RANDOM_BARCODE_LOCATION=', "'", param[['RANDOM_BARCODE_LOCATION']], "'"),
+        paste0('KEEP_RANDOM_BARCODE_STATS=', param[['KEEP_RANDOM_BARCODE_STATS']]),
         
         ## Alignment options
-        paste0('STAR_alignEndsType=',param[['STAR_alignEndsType']]),                            
-        paste0('STAR_outFilterMatchNmin=',param[['STAR_outFilterMatchNmin']]),                   
-        paste0('STAR_outFilterMatchNminOverLread=',param[['STAR_outFilterMatchNminOverLread']]), 
-        paste0('STAR_outFilterMismatchNmax=',param[['STAR_outFilterMismatchNmax']]),            
+        paste0('STAR_alignEndsType=', param[['STAR_alignEndsType']]),
+        paste0('STAR_outFilterMatchNmin=', param[['STAR_outFilterMatchNmin']]),
+        paste0('STAR_outFilterMatchNminOverLread=', param[['STAR_outFilterMatchNminOverLread']]),
+        paste0('STAR_outFilterMismatchNmax=', param[['STAR_outFilterMismatchNmax']]),
         
         ## output options
-        paste0('REMOVE_LARGE_INTERMEDIATE_FILES=',param[['REMOVE_LARGE_INTERMEDIATE_FILES']])
+        paste0('REMOVE_LARGE_INTERMEDIATE_FILES=', param[['REMOVE_LARGE_INTERMEDIATE_FILES']])
   )
   return(cmd)
 }
