43bf35c70bc5c62c19c9ebd2d86e74a6f271e2f9
Lennart Opitz <lopitz@fgcz.ethz.ch>
2020-07-06 10:51:44 +0200

    run mapping only ones in SpaceCounterApp

--- R/app-countSpacer.R
+++ R/app-countSpacer.R
@@ -10,6 +10,7 @@ ezMethodCountSpacer = function(input=NA, output=NA, param=NA){
   require(ShortRead)
   require(ggplot2)
   require(htmlwidgets)
+  require(stringi)
   
   setwdNew(param[['name']])
   sampleName = input$getNames()
@@ -17,8 +18,12 @@ ezMethodCountSpacer = function(input=NA, output=NA, param=NA){
   dict = ezRead.table(param[['dictPath']], header = FALSE, sep = ',', row.names = NULL)
   colnames(dict) = c('TargetID', 'Sequence', 'GeneSymbol', 'isControl')
   dict[['ID']] = paste(dict$TargetID, dict$Sequence, sep = '-')
+  stats = list()
+  stats[['rawReads']] = as.numeric(input$meta[['Read Count']])
+  
   trimmedInput = ezMethodTrim(input = input, param = param)
   readFile = trimmedInput$getColumn("Read1")
+  stats[['filteredReads']] = as.numeric(ezSystem(paste('cat', readFile, '|wc -l'), intern = TRUE))/4
   
   reads <- .getReadsFromFastq(readFile)
   reads <- twoPatternReadFilter(reads, param$leftPattern, param$rightPattern, param$maxMismatch)
@@ -26,52 +31,49 @@ ezMethodCountSpacer = function(input=NA, output=NA, param=NA){
   ###Export as fasta file
   readFile = paste0(sampleName,'.fa')
   reads = reads[width(reads) >= param$minReadLength]
+  stats[['validSpacerReads']] = length(reads)
   writeXStringSet(reads, readFile, format = 'fasta')
   
-  ###Run Bowtie1:
-  resultFile_0MM = paste0(sampleName, '_0MM.txt')
-  cmd = paste('bowtie', sub('.csv', '', param[['dictPath']]), readFile, '-f -p', param$cores, '-v 0|cut -f3|sort|uniq -c >', resultFile_0MM)
+  ###Run Bowtie1: ###TODO: runBowtie1 only 1 time (combined BAM) -> filter reads for MM
+  resultFile = paste0(sampleName, '_bowtie.txt')
+  cmd = paste('bowtie', sub('.csv', '', param[['dictPath']]), readFile, '-f -p', param$cores, '|cut -f3,8|sort >', resultFile)
   ezSystem(cmd)
-  result_0MM = ezRead.table(resultFile_0MM, row.names = NULL, header = FALSE)
-  result_0MM = data.frame(limma::strsplit2(trimws(result_0MM$V1), ' '), stringsAsFactors = FALSE)
-  colnames(result_0MM) = c('Count_0MM', 'ID')
-  result_0MM$Count_0MM = as.numeric(result_0MM$Count_0MM)
+  result_bowtie = ezRead.table(resultFile, row.names = NULL, header = FALSE)
+  colnames(result_bowtie) = c('target', 'mismatches')
+  result_bowtie$mismatches[result_bowtie$mismatches == ''] = 0
+  result_bowtie$mismatches[which(result_bowtie$mismatches != 0)] = stri_count_regex(result_bowtie$mismatches[which(result_bowtie$mismatches != 0)], '>')
+  result_bowtie$mismatches = as.numeric(result_bowtie$mismatches)
+  stats[['sgRNA_hits_0MM']] = length(which(result_bowtie$mismatches == 0))
+  stats[['sgRNA_hits_1MM']] = length(which(result_bowtie$mismatches == 1))
+  stats[['sgRNA_hits_2MM']] = length(which(result_bowtie$mismatches == 2))
+  stats[['sgRNA_hits_3MM']] = length(which(result_bowtie$mismatches == 3))
   
-  resultFile_1MM = paste0(sampleName, '_1MM.txt')
-  cmd = paste('bowtie', sub('.csv', '', param[['dictPath']]), readFile, '-f -p', param$cores, '-v 1|cut -f3|sort|uniq -c >', resultFile_1MM)
-  ezSystem(cmd)
-  result_1MM = ezRead.table(resultFile_1MM, row.names = NULL, header = FALSE)
-  result_1MM = data.frame(limma::strsplit2(trimws(result_1MM$V1), ' '), stringsAsFactors = FALSE)
-  colnames(result_1MM) = c('Count_1MM', 'ID')
-  result_1MM$Count_1MM = as.numeric(result_1MM$Count_1MM)
-  
-  result  = merge(result_0MM, result_1MM, by.x = 'ID', by.y = 'ID')
-  result$Count_0MM = as.numeric(result$Count_0MM)
-  result$Count_1MM = as.numeric(result$Count_1MM)
+  result = data.frame(table(result_bowtie$target), stringsAsFactors = FALSE)
+  colnames(result) = c('ID', 'Count')
+  result$Count = as.numeric(result$Count)
   dict = merge(dict, result, by.x = 'ID', by.y = 'ID', all.x = TRUE)
-  dict[is.na(dict$Count_0MM), 'Count_0MM'] = 0
-  dict[is.na(dict$Count_1MM), 'Count_1MM'] = 0
+  dict[is.na(dict$Count), 'Count'] = 0
   
   ###Export Table
   ezWrite.table(dict, paste0(sampleName,'-result.txt') ,row.names = FALSE)
-  data = data.frame(group = c(rep('Count_0MM', nrow(dict)), rep('Count_1MM', nrow(dict))), counts = c(dict$Count_0MM, dict$Count_1MM))
+  data = data.frame(group = rep('Count', nrow(dict)), counts = c(dict$Count))
   # Basic violin plot
   p <- ggplot(data, aes(x=group, y=counts))
   p <- p + geom_violin(fill="royalblue", alpha= 0.5, trim = FALSE, adjust = 0.5) 
   p <- p + geom_boxplot(width = 0.1)
   p <- p +  ggtitle(paste0(sampleName, '-ReadCount Distribution')) + ylab('ReadCount per sgRNA')
-  p <- p + theme(plot.title = element_text(size=15, face="bold"), axis.title.x =element_blank(), axis.text.x = element_text(angle=45,hjust=1))
+  p <- p + theme(plot.title = element_text(size = 15, face = "bold"), axis.title.x = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
   png(paste0(sampleName, '_violin.png'), 600, 1000, res = 100)
-  print(p)
+    print(p)
   dev.off()
   
-  h <- ggplot(dict, aes(x=log2(1+Count_0MM))) + geom_histogram(binwidth=0.1)
-  h <- h +  ggtitle(paste0(sampleName, '-Histogram 0MM')) + ylab('Number of sgRNAs') + xlab('Log2 count per sgRNA (0MM)')
+  h <- ggplot(dict, aes(x=log2(1+Count))) + geom_histogram(binwidth=0.1)
+  h <- h +  ggtitle(paste0(sampleName, '-Histogram')) + ylab('Number of sgRNAs') + xlab('Log2 count per sgRNA')
   png(paste0(sampleName, '_histogram.png'), 800, 500, res = 100)
-  print(h)
+    print(h)
   dev.off()
   
-  sortedCounts = log2(1+sort(dict$Count_0MM))
+  sortedCounts = log2(1+sort(dict$Count))
   upperCutOff = 2+mean(sortedCounts)
   lowerCutOff = mean(sortedCounts)-2
   
@@ -95,10 +97,10 @@ ezMethodCountSpacer = function(input=NA, output=NA, param=NA){
   targets = unique(dict2$TargetID)
   targetView = data.frame(TargetID = targets, stringsAsFactors = FALSE)
   targetView[['GeneSymbol']] = tapply(dict2$GeneSymbol, dict2$TargetID, unique)
-  targetView[['Count_0MM']] = tapply(dict2$Count_0MM, dict2$TargetID, paste, collapse = ',')
-  targetView[['Count_0MM_Sum']] = tapply(dict2$Count_0MM, dict2$TargetID, sum)
-  targetView[['#sgRNAs > 0']] = tapply(dict2$Count_0MM, dict2$TargetID, .greaterMin, 0)
-  targetView[['#sgRNAs > lowerCutOff']] = tapply(dict2$Count_0MM, dict2$TargetID, .greaterMin, 2^lowerCutOff)
+  targetView[['Count']] = tapply(dict2$Count, dict2$TargetID, paste, collapse = ',')
+  targetView[['Count_Sum']] = tapply(dict2$Count, dict2$TargetID, sum)
+  targetView[['#sgRNAs > 0']] = tapply(dict2$Count, dict2$TargetID, .greaterMin, 0)
+  targetView[['#sgRNAs > lowerCutOff']] = tapply(dict2$Count, dict2$TargetID, .greaterMin, 2^lowerCutOff)
   targetView = targetView[order(targetView[['#sgRNAs > lowerCutOff']]),]
   
   underrepTargets = targetView[targetView[['#sgRNAs > lowerCutOff']] < 2,]
@@ -110,7 +112,7 @@ ezMethodCountSpacer = function(input=NA, output=NA, param=NA){
                            "function(settings, json) {",
                            "$(this.api().table().header()).css({'background-color': '#0000A0', 'color': '#fff'});",
                            "}"),
-                           dom = c('Bfrtip'),buttons = c('colvis','copy', 'csv', 'excel', 'pdf', 'print'), pageLength=100, autoWidth=TRUE))
+                           dom = c('Bfrtip'),buttons = c('colvis','copy', 'csv', 'excel', 'pdf', 'print'), pageLength = 100, autoWidth=TRUE))
     saveWidget(myDT, 'underrepresentedTargets.html')
   }
   ezWrite.table(targetView, paste0(sampleName,'-targetBasedResult.txt') ,row.names = FALSE)
@@ -124,6 +126,7 @@ ezMethodCountSpacer = function(input=NA, output=NA, param=NA){
   rmarkdown::render(input="CountSpacer.Rmd", envir = new.env(),
                     output_dir=".", output_file=htmlFile, quiet=TRUE)
   
+  ezWrite.table(unlist(stats), paste0(sampleName,'-stats.txt'), row.names = TRUE)
   remove(reads)
   ezSystem('rm *.fastq')
   ezSystem('pigz --best *.fa')
