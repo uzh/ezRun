7542ff35d51f46d980020c4f014fd16ab2323b59
Daymé González Rodríguez <daymegr@fgcz-net.unizh.ch>
2020-08-13 16:38:28 +0200

    SCT assay is better than RNA for visualization

--- R/app-SCMultipleSamplesAndGroups.R
+++ R/app-SCMultipleSamplesAndGroups.R
@@ -145,6 +145,9 @@ ezMethodSCMultipleSamplesAndGroups = function(input=NA, output=NA, param=NA, htm
   }
   
   #Convert scData to Single Cell experiment Object
+  # Use the SCT logcounts for visualization instead of the RNA logcounts.
+  # TODO: save all the assays (RNA, SCT and integrated) in the sce object using the package keshavmot2/scanalysis. The function from Seurat doesn't save everything.
+  DefaultAssay(scData) <- "SCT" 
   sce <- as.SingleCellExperiment(scData)
   metadata(sce)$cells_AUC <- cells_AUC
   metadata(sce)$singler.results <- singler.results
