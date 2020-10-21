eb35a3b403df59f382b3c3365551f693d2d9e818
Hubert Rehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2020-10-21 16:38:38 +0200

    suppress coltypes messages

--- R/ngsio.r
+++ R/ngsio.r
@@ -16,7 +16,7 @@ loadCountDataset <- function(input, param){
   dataFeatureLevel <- unique(input$getColumn("featureLevel"))
   stopifnot(length(dataFeatureLevel) == 1)
   
-  x1 <- read_tsv(files[1], guess_max=1e6)
+  x1 <- read_tsv(files[1], guess_max=1e6,  col_types = cols())
   
   if (ezIsSpecified(param$expressionName)){
     columnName <- param$expressionName
@@ -33,7 +33,7 @@ loadCountDataset <- function(input, param){
   
   x <- mapply(function(x, y){
     message("loading file: ", x)
-    tempTibble <- read_tsv(x, progress=FALSE, guess_max=1e6)
+    tempTibble <- read_tsv(x, progress=FALSE, guess_max=1e6, col_types = cols())
     tempTibble %>%
       dplyr::select(identifier, columnName) %>%
       dplyr::rename("id":= 1, !! y := columnName)
