1726d31a071c314472541eaae204b210a06bcf64
Ge Tan <gtan@me.com>
2020-12-01 17:01:16 +0100

    CellRangerAggr: path fix

--- R/app-cellRangerAggr.R
+++ R/app-cellRangerAggr.R
@@ -68,7 +68,7 @@ ezMethodCellRangerAggr <- function(input = NA, output = NA, param = NA) {
     select(-SampleID)
   write_tsv(cellPhaseAggr,
     file = file.path(
-      param$name, "filtered_feature_bc_matrix", "CellCyclePhase.txt"
+      param$name, "filtered_feature_bc_matrix", "count", "CellCyclePhase.txt"
     )
   )
   return("Success")
