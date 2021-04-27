80c52dd40ffe96d90390737fed834d814c7b36da
falkonoe <falnoe@ethz.ch>
2021-04-27 15:16:48 +0200

    CountQC no longer produces additional PNG or PDF files

--- R/plots-reports.R
+++ R/plots-reports.R
@@ -18,102 +18,9 @@
 ##' @param isPresentCond Either NULL or a data.frame containing coloring information.
 ##' @template types-template
 ##' @template roxygen-template
-addQcScatterPlots = function(doc, param, design, conds, rawData, signalCond, isPresentCond, types=NULL){
-  samples = rownames(design)
-  nConds = length(unique(conds))
-  signal = getSignal(rawData)
-  signal[signal <= 0] = NA
-  isPresent = ezPresentFlags(signal, presentFlag=assays(rawData)$presentFlag, 
-                             param=param, isLog=metadata(rawData)$isLog)
-  signalRange = range(signal, na.rm=TRUE)
-  qcScatterTitles = list()
-  qcScatterTitles[["Scatter Plots by Conditions"]] = "Scatter Plots by Conditions"
-  addTitle(doc, qcScatterTitles[[length(qcScatterTitles)]], 2, id=qcScatterTitles[[length(qcScatterTitles)]])
-  if (!is.null(rowData(rawData)$gc)){
-    gcTypes = data.frame("GC < 0.4"=as.numeric(rowData(rawData)$gc) < 0.4,
-                         "GC > 0.6"=as.numeric(rowData(rawData)$gc) > 0.6,
-                         check.names=FALSE)
-  } else {
-    gcTypes = NULL
-  }
-  if (!is.null(rowData(rawData)$featWidth)){
-    widthTypes = data.frame("width < 500nt"=as.numeric(rowData(rawData)$featWidth) < 500, 
-                            "width > 5000nt"=as.numeric(rowData(rawData)$featWidth) > 5000,
-                            check.names=FALSE)
-  } else {
-    widthTypes = NULL
-  }
-  if (nConds > 1 & nConds <=  param$allPairsMaxCondNumber){
-    plotCmd = expression({
-      ezAllPairScatter(signalCond, isPresent=isPresentCond, types=types)
-    })
-    imgLinks = character()
-    imgLinks["def"] = ezImageFileLink(plotCmd, file="allPairs-scatter.png",
-                                      width=min(max(ncol(signalCond) * 200, 400), 2000),
-                                      height=min(max(ncol(signalCond) * 200, 400), 2000)) # dynamic png with possibly many plots
-    if (!is.null(gcTypes)){
-      plotCmd = expression({
-        ezAllPairScatter(signalCond, main="color by GC", isPresent=isPresentCond, types=gcTypes)
-      })
-      imgLinks["gc"] = ezImageFileLink(plotCmd, file="allPairs-scatter-byGc.png",
-                                       width=min(max(ncol(signalCond) * 200, 400), 2000),
-                                       height=min(max(ncol(signalCond) * 200, 400), 2000)) # dynamic png with possibly many plots
-    }
-    if (!is.null(widthTypes)){
-      plotCmd = expression({
-        ezAllPairScatter(signalCond, main="color by width", isPresent=isPresentCond, types=widthTypes)
-      })
-      imgLinks["width"] = ezImageFileLink(plotCmd, file="allPairs-scatter-byWidth.png",
-                                          width=min(max(ncol(signalCond) * 200, 400), 2000),
-                                          height=min(max(ncol(signalCond) * 200, 400), 2000)) # dynamic png with possibly many plots
-    }
-    addFlexTable(doc, ezGrid(t(imgLinks)))
-  }
-  for (i in 1:min(4, ncol(design))){
-    for (cond in unique(design[,i])){
-      idx = which(cond == design[,i])
-      if (length(idx) > 1){
-        idx = idx[order(samples[idx])] ## order alphabetically
-        condName = paste(colnames(design)[i], cond)
-        nPlots = length(idx)
-        if (nPlots == 2) nPlots = 1
-        qcScatterTitles[[paste(condName, cond, i)]] = condName
-        addTitle(doc, qcScatterTitles[[length(qcScatterTitles)]], 3, id=qcScatterTitles[[length(qcScatterTitles)]])
-        pngName = ezValidFilename(paste0(condName, "-scatter.png"))
-        plotCmd = expression({
-          ezScatter(y=signal[ ,idx], isPresent=isPresent[ ,idx], types=types, lim=signalRange, xlab=paste("Avg of", cond), ylab=NULL)
-        })
-        imgLinks = character()
-        imgLinks["def"] = ezImageFileLink(plotCmd, file=pngName,
-                                  width=min(nPlots, 6) * 300,
-                                  height=ceiling(nPlots/6) * 330) # dynamic png with possibly many plots
-        if (!is.null(gcTypes)){
-          pngName = ezValidFilename(paste0(condName, "-ByGcScatter.png"))
-          plotCmd = expression({
-            ezScatter(y=signal[ ,idx], isPresent=isPresent[ ,idx], types=gcTypes, lim=signalRange, xlab=paste("Avg of", cond), ylab=NULL)
-          })
-          imgLinks["gc"] = ezImageFileLink(plotCmd, file=pngName,
-                                   width=min(nPlots, 6) * 300,
-                                   height=ceiling(nPlots/6) * 330) # dynamic png with possibly many plots
-        }
-        if (!is.null(widthTypes)){
-          pngName = ezValidFilename(paste0(condName, "-ByWidthScatter.png"))
-          plotCmd = expression({
-            ezScatter(y=signal[ ,idx], isPresent=isPresent[ ,idx], types=widthTypes, lim=signalRange, xlab=paste("Avg of", cond), ylab=NULL)
-          })
-          imgLinks["width"] = ezImageFileLink(plotCmd, file=pngName,
-                                      width=min(nPlots, 6) * 300,
-                                      height=ceiling(nPlots/6) * 330) # dynamic png with possibly many plots
-        }
-        addFlexTable(doc, ezGrid(imgLinks))
-      }
-    }
-  }
-  return(qcScatterTitles)
-}
-
 countQcScatterPlots = function(param, design, conds, rawData, signalCond, 
                                isPresentCond, types=NULL){
+  
   samples = rownames(design)
   nConds = length(unique(conds))
   signal = getSignal(rawData) + param$backgroundExpression
@@ -138,85 +45,67 @@ countQcScatterPlots = function(param, design, conds, rawData, signalCond,
     widthTypes = NULL
   }
   
-  plotsToInclude <- list()
-  
+  widePlots = c()
   if (nConds > 1 & nConds <=  param$allPairsMaxCondNumber){
-    plotCmd = expression({
-      ezAllPairScatter(signalCond, isPresent=isPresentCond, types=types)
-    })
-    imgLinks = character()
-    imgLinks["def"] = ezImageFileLink(plotCmd, file="allPairs-scatter.png",
-                                      width=min(max(ncol(signalCond) * 200, 400), 2000),
-                                      height=min(max(ncol(signalCond) * 200, 400), 2000)) # dynamic png with possibly many plots
-    plotsToInclude[["allPairs"]] <- c()
-    plotsToInclude[["allPairs"]] <- c(plotsToInclude[["allPairs"]], "allPairs-scatter.png")
-    
-    if (!is.null(gcTypes)){
-      plotCmd = expression({
-        ezAllPairScatter(signalCond, main="color by GC", 
-                         isPresent=isPresentCond, types=gcTypes)
+    widePlots[["allPairs"]] <- c(
+      expression({
+        ezAllPairScatter(signalCond, isPresent=isPresentCond, types=types)
       })
-      imgLinks["gc"] = ezImageFileLink(plotCmd, file="allPairs-scatter-byGc.png",
-                                       width=min(max(ncol(signalCond) * 200, 400), 2000),
-                                       height=min(max(ncol(signalCond) * 200, 400), 2000)) # dynamic png with possibly many plots
-      plotsToInclude[["allPairs"]] <- c(plotsToInclude[["allPairs"]], 
-                                        "allPairs-scatter-byGc.png")
+    )
+    if (!is.null(gcTypes)){
+      widePlots[["allPairs"]] <- c(
+        widePlots[["allPairs"]], 
+        expression({
+          ezAllPairScatter(signalCond, main="color by GC", isPresent=isPresentCond, types=gcTypes)
+        })
+      )
     }
     if (!is.null(widthTypes)){
-      plotCmd = expression({
-        ezAllPairScatter(signalCond, main="color by width", 
-                         isPresent=isPresentCond, types=widthTypes, colors = widthColors)
-      })
-      imgLinks["width"] = ezImageFileLink(plotCmd, file="allPairs-scatter-byWidth.png",
-                                          width=min(max(ncol(signalCond) * 200, 400), 2000),
-                                          height=min(max(ncol(signalCond) * 200, 400), 2000)) # dynamic png with possibly many plots
-      plotsToInclude[["allPairs"]] <- c(plotsToInclude[["allPairs"]], 
-                                        "allPairs-scatter-byWidth.png")
+      widePlots[["allPairs"]] <- c(
+        widePlots[["allPairs"]], 
+        expression({
+          ezAllPairScatter(signalCond, main="color by width", isPresent=isPresentCond, types=widthTypes, colors = widthColors)
+        })
+      )
     }
   }
+  narrowPlots = c()
   for (i in 1:min(4, ncol(design))){
     for (cond in unique(design[,i])){
       idx = which(cond == design[,i])
       if (length(idx) > 1){
         idx = idx[order(samples[idx])] ## order alphabetically
         condName = paste(colnames(design)[i], cond)
-        plotsToInclude[[condName]] <- c()
+        narrowPlots[[condName]] <- c()
         nPlots = length(idx)
         if (nPlots == 2) nPlots = 1
-        pngName = ezValidFilename(paste0(condName, "-scatter.png"))
-        plotCmd = expression({
-          ezScatter(y=signal[ ,idx], isPresent=isPresent[ ,idx], types=types, lim=signalRange, xlab=paste("Avg of", cond), ylab=NULL)
-        })
-        imgLinks = character()
-        imgLinks["def"] = ezImageFileLink(plotCmd, file=pngName,
-                                          width=min(nPlots, 6) * 300,
-                                          height=ceiling(nPlots/6) * 330) # dynamic png with possibly many plots
-        plotsToInclude[[condName]] <- c(plotsToInclude[[condName]], pngName)
-        
-        if (!is.null(gcTypes)){
-          pngName = ezValidFilename(paste0(condName, "-ByGcScatter.png"))
-          plotCmd = expression({
-            ezScatter(y=signal[ ,idx], isPresent=isPresent[ ,idx], types=gcTypes, lim=signalRange, xlab=paste("Avg of", cond), ylab=NULL)
+        narrowPlots[[condName]] <- c(
+          narrowPlots[[condName]], 
+          expression({
+            ezScatter(y=signal[ ,idx], isPresent=isPresent[ ,idx], types=types, lim=signalRange, xlab=paste("Avg of", cond), ylab=NULL)
           })
-          imgLinks["gc"] = ezImageFileLink(plotCmd, file=pngName,
-                                           width=min(nPlots, 6) * 300,
-                                           height=ceiling(nPlots/6) * 330) # dynamic png with possibly many plots
-          plotsToInclude[[condName]] <- c(plotsToInclude[[condName]], pngName)
+        )
+
+        if (!is.null(gcTypes)){
+          narrowPlots[[condName]] <- c(
+            narrowPlots[[condName]], 
+            expression({
+              ezScatter(y=signal[ ,idx], isPresent=isPresent[ ,idx], types=gcTypes, lim=signalRange, xlab=paste("Avg of", cond), ylab=NULL)
+            })
+          )
         }
         if (!is.null(widthTypes)){
-          pngName = ezValidFilename(paste0(condName, "-ByWidthScatter.png"))
-          plotCmd = expression({
-            ezScatter(y=signal[ ,idx], isPresent=isPresent[ ,idx], types=widthTypes, lim=signalRange, xlab=paste("Avg of", cond), ylab=NULL, colors=widthColors)
-          })
-          imgLinks["width"] = ezImageFileLink(plotCmd, file=pngName,
-                                              width=min(nPlots, 6) * 300,
-                                              height=ceiling(nPlots/6) * 330) # dynamic png with possibly many plots
-          plotsToInclude[[condName]] <- c(plotsToInclude[[condName]], pngName)
+          narrowPlots[[condName]] <- c(
+            narrowPlots[[condName]], 
+            expression({
+              ezScatter(y=signal[ ,idx], isPresent=isPresent[ ,idx], types=widthTypes, lim=signalRange, xlab=paste("Avg of", cond), ylab=NULL, colors=widthColors)
+            })
+          )
         }
       }
     }
   }
-  return(plotsToInclude)
+  return(list(widePlots=widePlots, narrowPlots=narrowPlots, envir=sys.frame(which=sys.nframe())))
 }
 
 
