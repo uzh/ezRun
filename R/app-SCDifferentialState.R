3380ad2e8598c24f19d182a5e5dc6ec0c95aeada
Daymé González Rodríguez <daymegr@fgcz-net.unizh.ch>
2021-08-26 16:56:06 +0200

    when subsetting the object, the clusters that are used change

--- R/app-SCDifferentialState.R
+++ R/app-SCDifferentialState.R
@@ -75,7 +75,7 @@ ezMethodSCDifferentialState = function(input=NA, output=NA, param=NA, htmlFile="
      #differentially expressed genes between clusters and conditions (in case of several conditions)
      diffGenes <- diffExpressedGenes(scData)
   }
-  dataFiles = saveExternalFiles(list(differential_genes=diffGenes, conserved_markers=consMarkers))
+  dataFiles = saveExternalFiles(list(differential_genes=diffGenes, conserved_markers=consMarkers, current_clusters=data.frame(Idents(scData))))
   saveRDS(input, "input.rds")
   saveRDS(param, "param.rds")
   
