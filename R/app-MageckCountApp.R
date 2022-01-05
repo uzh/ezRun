b32634e647f11c433e1f7541631d3ef9848e99fb
Lennart Opitz <lopitz@fgcz.ethz.ch>
2022-01-05 14:37:08 +0100

    automated reference creation for MaGeck

--- R/app-MageckCountApp.R
+++ R/app-MageckCountApp.R
@@ -5,15 +5,53 @@
 # The terms are available here: http://www.gnu.org/licenses/gpl.html
 # www.fgcz.ch
 
-
 ezMethodMageckCount <- function(input, output, param){
     require(Herper)
     local_CondaEnv("mageckenv", pathToMiniConda = "/usr/local/ngseq/miniconda3")
     sampleName <- input$getNames()
     inputFile  <- input$getFullPaths("Read1")
-    param[['dictPath']] <- list.files(param[['libName']], pattern = 'MAGeCK.csv$', full.names = TRUE)
-    ##TODO: handle empty dictPath
-    system2("mageck", args = c("count", "-l", param[['dictPath']], "--fastq", inputFile, "-n", sampleName))
+    param <- getMageckReference(param)
+    if(identical(param[['ctrlFile']], character(0))){
+        system2("mageck", args = c("count", "-l", param[['dictFile']], "--fastq", inputFile, "-n", sampleName))
+    } else {
+        system2("mageck", args = c("count", "-l", param[['dictFile']], "--control-sgrna", param[['ctrlFile']], "--fastq", inputFile, "-n", sampleName))
+    }
+}
+
+getMageckReference <- function(param){
+    param[['dictFile']] <- list.files(param[['libName']], pattern = 'MAGeCK.csv$', full.names = TRUE)
+    param[['ctrlFile']] <- list.files(param[['libName']], pattern = 'MAGeCK_Ctrl.csv$', full.names = TRUE)
+    
+    lockFile <- file.path(param[['libName']], "lock")
+    if(identical(param[['dictFile']], character(0)) & !file.exists(lockFile)){
+        ###Create LockFile
+        ezWrite(Sys.info(), con = lockFile)
+        
+        ###FIND csv file
+        basicFile <- list.files(param[['libName']], pattern = '.csv$', full.names = TRUE)
+        if(length(basicFile) == 1L){
+            myRef <- ezRead.table(basicFile, row.names = NULL, sep = ',', header = FALSE)
+            colnames(myRef) <- c('TranscriptName', 'Sequence', 'GeneSymbol', 'isControl')
+            
+            myRef[['ID']] <- paste(myRef[['TranscriptName']],myRef[['Sequence']], sep = '_')
+            myRefList <- split(myRef, f = myRef$isControl)
+            refFile <- sub('.csv', '_MAGeCK.csv', basicFile)
+            ezWrite.table(myRef[,c('ID', 'Sequence', 'GeneSymbol')], refFile, col.names = FALSE, row.names = FALSE, sep = ',')
+            param[['dictFile']] <- refFile
+            if(length(myRefList) == 2L){
+                ctrlFile <- sub('.csv', '_MAGeCK_Ctrl.csv', basicFile)
+                ezWrite.table(myRefList[['TRUE']][,c('ID')], ctrlFile, col.names = FALSE, row.names = FALSE)
+                param[['ctrlFile']] <- ctrlFile
+            } else {
+                param[['ctrlFile']] <- list.files(param[['libName']], pattern = 'MAGeCK_Ctrl.csv$', full.names = TRUE)
+            }
+        } else {
+            file.remove(lockFile)
+            stop('no or multiple basic reference file(s) available')
+        }
+        file.remove(lockFile)
+    }
+    return(param)
 }
 
 ##' @template app-template
