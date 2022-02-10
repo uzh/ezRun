d341f719fda6e9539513056a1cc4f1d42b8e1848
daymegr <daymegr@fgcz.uzh.ch>
2022-02-10 11:19:19 +0100

    add app to merge SC samples with Seurat

---
+++ R/app-SCMultipleSamplesSeurat.R
@@ -0,0 +1,138 @@
+###################################################################
+# Functional Genomics Center Zurich
+# This code is distributed under the terms of the GNU General
+# Public License Version 3, June 2007.
+# The terms are available here: http://www.gnu.org/licenses/gpl.html
+# www.fgcz.ch
+
+EzAppSCMultipleSamplesSeurat <-
+  setRefClass("EzAppSCMultipleSamplesSeurat",
+              contains = "EzApp",
+              methods = list(
+                initialize = function()
+                {
+                  "Initializes the application using its specific defaults."
+                  runMethod <<- ezMethodSCMultipleSamplesSeurat
+                  name <<- "EzAppSCMultipleSamplesSeurat"
+                  appDefaults <<- rbind(npcs=ezFrame(Type="numeric", 
+                                                    DefaultValue=30, 
+                                                    Description="The maximal dimensions to use for reduction"),
+                                        pcGenes = ezFrame(
+                                          Type = "charVector",
+                                          DefaultValue = "",
+                                          Description = "The genes used in supvervised clustering"),
+                                        resolution=ezFrame(Type="numeric", 
+                                                           DefaultValue=0.6,
+                                                           Description="Value of the resolution parameter, use a value above (below) 1.0 if you want to obtain a larger (smaller) number of communities."),
+                                        batchCorrection=ezFrame(Type="logical", 
+                                                                DefaultValue="TRUE",
+                                                                Description="Perform batch correction."),
+                                        SCT.regress=ezFrame(Type="character", 
+                                                            DefaultValue="none", 
+                                                            Description="Choose CellCycle to be regressed out when using the SCTransform method if it is a bias."),
+                                        DE.method=ezFrame(Type="charVector", 
+                                                          DefaultValue="wilcox", 
+                                                          Description="Method to be used when calculating gene cluster markers and differentially expressed genes between conditions. Use LR to take into account the Batch and/or CellCycle"),
+                                        DE.regress=ezFrame(Type="charVector", 
+                                                           DefaultValue="Batch", 
+                                                           Description="Variables to regress out if the test LR is chosen"),
+                                        maxSamplesSupported=ezFrame(Type="numeric", 
+                                                              DefaultValue=5, 
+                                                              Description="Maximum number of samples to compare"))
+                }
+              )
+  )
+
+ezMethodSCMultipleSamplesSeurat = function(input=NA, output=NA, param=NA, htmlFile="00index.html") {
+  library(Seurat)
+  library(rlist)
+  library(HDF5Array)
+  library(scanalysis)
+  library(SummarizedExperiment)
+  library(SingleCellExperiment)
+  
+
+  if(input$getLength() > param$maxSamplesSupported){
+    stop(paste("It only works for", param$maxSamplesSupported, "at most"))
+  }
+  
+  cwd <- getwd()
+  setwdNew(basename(output$getColumn("Report")))
+  on.exit(setwd(cwd), add=TRUE)
+  reportCwd <- getwd()
+  
+  #the individual sce objects can be in hdf5 format (for new reports) or in rds format (for old reports)
+  filePath <- paste0("/srv/gstore/projects/", input$getColumn("SC Seurat"))
+  filePath_course <- paste0("/srv/GT/analysis/course_sushi/public/projects/", input$getColumn("SC Seurat"))
+  
+  if(!file.exists(filePath)) 
+    filePath <- filePath_course
+  
+  scDataList <- lapply(filePath, readRDS)
+  names(scDataList) <- names(input$getColumn("Static Report"))
+  
+  pvalue_allMarkers <- 0.05
+  nrSamples <- length(scDataList)
+  
+  if(ezIsSpecified(param$chosenClusters)){
+    for(eachSample in names(param$chosenClusters)){
+      chosenCells <- names(Idents(scDataList[[eachSample]]))[Idents(scDataList[[eachSample]]) %in% param$chosenClusters[[eachSample]]]
+      scDataList[[eachSample]] <- scDataList[[eachSample]][, chosenCells]
+       scDataList[[eachSample]] <-
+        SubsetData(scDataList[[eachSample]],
+                   ident.use=param$chosenClusters[[eachSample]])
+    }
+  }
+  
+  scData_noCorrected <- cellClustNoCorrection(scDataList, param)
+  scData = scData_noCorrected
+  if (param$batchCorrection) {
+    scData_corrected = cellClustWithCorrection(scDataList, param)
+    #in order to compute the markers we switch again to the original assay
+    DefaultAssay(scData_corrected) <- "SCT"
+    scData <- scData_corrected
+  }
+  scData@reductions$tsne_noCorrected <- Reductions(scData_noCorrected, "tsne")
+  scData@reductions$umap_noCorrected <- Reductions(scData_noCorrected, "umap")
+  scData@meta.data$ident_noCorrected <- Idents(scData_noCorrected)
+  
+  #positive cluster markers
+  posMarkers <- posClusterMarkers(scData, pvalue_allMarkers, param)
+  
+  #we do cell type identification using AUCell and SingleR
+  cells_AUC <- NULL
+  singler.results <- NULL
+  #cell types annotation is only supported for Human and Mouse at the moment
+  species <- getSpecies(param$refBuild)
+  if(species == "Human" | species == "Mouse") {
+    cells.AUC = cellsLabelsWithAUC(scData, species, param$tissue)
+    singler.results <- cellsLabelsWithSingleR(GetAssayData(scData, "counts"), Idents(scData), species)
+  }
+  
+  geneMeans <- geneMeansCluster(scData)
+  
+  saveRDS(cells.AUC, file="cells.AUC.rds")
+  saveRDS(singler.results, file="singler.results.rds")
+  saveRDS(param, file="param.rds")
+  saveRDS(output, file="output.rds")
+  saveRDS(scData, file = "scData.rds")
+
+  #object to be used in isee
+  #TODO: remove unnecesary dietseurat call when the bug in Seurat is fixed
+  scData_diet = DietSeurat(scData, dimreducs = c("pca", "tsne", "umap"))
+  sce <- scData_diet %>% seurat_to_sce(default_assay = "SCT")
+  saveHDF5SummarizedExperiment(sce, dir = "sce_h5")
+  
+  # Save some results in external files
+  dataFiles <- saveExternalFiles(list(pos_markers = posMarkers, gene_means = as_tibble(as.data.frame(geneMeans), rownames = "gene_name")))
+  
+  makeRmdReport(dataFiles=dataFiles, rmdFile = "SCMultipleSamplesSeurat.Rmd", reportTitle = param$name) 
+  return("Success")
+  
+}
+
+
+
+
+
+
