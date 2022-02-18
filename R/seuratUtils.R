9fa08b33a26757f56fbf4aab1dd0d78f2239a1ce
daymegr <daymegr@fgcz.uzh.ch>
2022-02-18 11:49:50 +0100

    use ident for current clusters

--- R/seuratUtils.R
+++ R/seuratUtils.R
@@ -146,7 +146,7 @@ seuratStandardWorkflow <- function(scData, param){
   scData <- FindNeighbors(object = scData, reduction = "pca", dims = 1:param$npcs, verbose=FALSE)
   scData <- FindClusters(object=scData, resolution = seq(from = 0.1, to = 1, by = 0.1), verbose=FALSE)  #calculate clusters for a set of resolutions
   Idents(scData) <- scData@meta.data[,paste0(DefaultAssay(scData), "_snn_res.", param$resolution)]  #but keep as the current clusters the ones obtained with the resolution set by the user
-  scData@meta.data$seurat_clusters <- Idents(scData)
+  scData@meta.data$ident <- Idents(scData)
   return(scData)
 }  
 
