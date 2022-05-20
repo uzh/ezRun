c1807ebc9f19f9b25825b76cfddcde7fda0e9d59
Natalia Zajac <natalia.zajac@fgcz.ethz.ch>
2022-05-20 14:33:52 +0200

    QIIME2

--- R/app-QIIME2.R
+++ R/app-QIIME2.R
@@ -23,7 +23,7 @@ ezMethodQIIME2 = function(input=NA, output=NA, param=NA,
   ###create sample metadata and manifest files
   if (!file.exists("sample_metadata.tsv")) {
     file.create("sample_metadata.tsv")
-    sample_metadata <- dataset[, 2, drop = FALSE]
+    sample_metadata <- dataset[, 1, drop = FALSE]
     sample_metadata$sample_id <- rownames(sample_metadata)
     sample_metadata <- sample_metadata[,c(2,1)]
     setnames(sample_metadata, "sample_id", "sample-id")
@@ -36,14 +36,14 @@ ezMethodQIIME2 = function(input=NA, output=NA, param=NA,
   if (!file.exists("manifest.tsv")) {
   
     file.create("manifest.tsv")
-    manifest <- dataset[, 1, drop = FALSE]
+    manifest <- dataset[, 2, drop = FALSE]
     manifest$sample_id <- rownames(manifest)
     manifest <- manifest[,c(2,1)]
     setnames(manifest, "sample_id", "sample-id")
     setnames(manifest, colnames(manifest)[2], "absolute-filepath")
     write_tsv(manifest, file = "manifest.tsv")
     if(isPaired){
-      manifest <- dataset[, c(1,3)]
+      manifest <- dataset[, c(2,3)]
       manifest$sample_id <- rownames(manifest)
       manifest <- manifest[,c(3,1,2)]
       setnames(manifest, "sample_id", "sample-id")
