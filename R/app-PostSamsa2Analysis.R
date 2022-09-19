6c16552ad42be979323116be8a97f9b4e9a6df7d
Natalia Zajac <zajac@fgcz-net.unizh.ch>
2022-09-19 13:49:07 +0200

    output files as rds

--- R/app-PostSamsa2Analysis.R
+++ R/app-PostSamsa2Analysis.R
@@ -53,10 +53,33 @@ ezMethodPostSamsa2Analysis = function(input=NA, output=NA, param=NA,
   samsa2RscriptsToBeExecCmd2 <- paste("bash /usr/local/ngseq/src/samsa2/R_scripts/run_all_Rscripts.sh func_results/")
   ezSystem(samsa2RscriptsToBeExecCmd2)
   
+  org_Shannon_Simpson <- readLines("org_results/Shannon_Simpson_diversity.txt")
+  func_Shannon_Simpson <- readLines("func_results/Shannon_Simpson_diversity.txt")
+  org_div_graph <- readRDS("org_results/diversity_graph.rds")
+  func_div_graph <- readRDS("func_results/diversity_graph.rds")
+  org_PCA <- readRDS("org_results/PCA_plot.tab.rds")
+  func_PCA <- readRDS("func_results/PCA_plot.tab.rds")
+  org_heatmap <- readRDS("org_results/DESeq_heatmap.rds")
+  func_heatmap <- readRDS("func_results/DESeq_heatmap.rds")
+  org_combined <- readRDS("org_results/combined_graph.rds")
+  func_combined <- readRDS("func_results/combined_graph.rds")
+  org_volcano <- readRDS("org_results/volcano_plot.rds")
+  func_volcano <- readRDS("func_results/volcano_plot.rds")
+  org_deseq_results <- read.csv("org_results/DESeq_results.tab", sep = "\t")
+  func_deseq_results <- read.csv("func_results/DESeq_results.tab", sep = "\t")
+  
   setwdNew(basename(output$getColumn("Report")))
   markdownFile <- "PostSamsa2.Rmd"
   ## Copy the style files and templates
-  makeRmdReport(param=param, output=output, rmdFile = markdownFile, reportTitle = "Samsa2 Report")
+  makeRmdReport(param=param, org_Shannon_Simpson = org_Shannon_Simpson,
+                func_Shannon_Simpson = func_Shannon_Simpson, org_div_graph = org_div_graph,
+                func_div_graph = func_div_graph, org_PCA = org_PCA,
+                func_PCA = func_PCA, org_heatmap = org_heatmap,
+                func_heatmap = func_heatmap, org_combined = org_combined,
+                func_combined = func_combined, org_volcano = org_volcano,
+                func_volcano = func_volcano, org_deseq_results = org_deseq_results,
+                func_deseq_results = func_deseq_results, output=output, 
+                rmdFile = markdownFile, reportTitle = "Samsa2 Report")
 }
 ##' @template app-template
 ##' @templateVar method ezMethodPostSamsa2Analysis()
