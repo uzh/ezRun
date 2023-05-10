52bdc497fa4cdcb797e1ff45b2982210502d6fb6
Weihong Qi <weihong.qi@fgcz.ethz.ch>
2023-05-10 09:24:27 +0200

    pbsv

--- R/app-pbsv.R
+++ R/app-pbsv.R
@@ -17,7 +17,6 @@ ezMethodPbsv <- function(input = NA, output = NA, param = NA) {
   ovzf = paste0(sampleName, ".vcf.gz")
   PbsvLogFile = paste0(sampleName, "_pbsv.log")
   PbsvStatsFile = paste0(sampleName, ".stats.txt")
-  PbsvStatsPlot = paste0(sampleName, ".stats.pdf")
   cmd = paste("/srv/GT/software/SMRTtools/SMRT_Link_v10/smrtcmds/bin/pbsv discover", param$afOptions)
   if(param$ReadOpt=="HIFI"){
 	    cmd=paste(cmd, "--hifi")
@@ -49,23 +48,21 @@ ezMethodPbsv <- function(input = NA, output = NA, param = NA) {
   indexTabix(basename(ovzf),format = "vcf")
   ezSystem(paste("SURVIVOR stats", ovf, "20 -1 3",  PbsvStatsFile)) 
 
-  plotPbsv(PbsvStatsFile, sampleName)
+  outPath<-param$outputDir
+  tbl<-read.table(paste0(outPath, "/", PbsvStatsFile), header=TRUE)
+  data_long<- gather(tbl, svType, count, Del:TRA, factor_key=TRUE)
+  data_long$Len<-factor(data_long$Len, levels = c("0-50bp", "50-100bp", "100-1000bp", "1000-10000bp", "10000+bp"))
+  p1<-ggplot(data=data_long, aes(x=Len, y=count, color=svType, fill=svType)) 
+  p1<-p1 + geom_bar(stat="identity") 
+  p1<-p1 + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
+  p1<-p1 + facet_grid(. ~ svType) + scale_y_continuous(trans='log10') + ggtitle(sampleName)
+  pdf(paste0(outPath, "/", sampleName, '.stats.pdf'))
+    	print(p1)
+  dev.off()
 
   return("Success")
 }
 
-plotPbsv <- function(PbsvStatsFile, sampleName){
-  	tbl<-read.table(PbsvStatsFile, header=TRUE)
-  	data_long<- gather(tbl, svType, count, Del:TRA, factor_key=TRUE)
-  	data_long$Len<-factor(data_long$Len, levels = c("0-50bp", "50-100bp", "100-1000bp", "1000-10000bp", "10000+bp"))
-  	p1<-ggplot(data=data_long, aes(x=Len, y=count, color=svType, fill=svType)) 
-  	p1<-p1 + geom_bar(stat="identity") 
-  	p1<-p1 + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
-  	p1<-p1 + facet_grid(. ~ svType) + scale_y_continuous(trans='log10') + ggtitle(sampleName)
-  	pdf(paste0(sampleName, '.stats.pdf'))
-    		print(p1)
-  	dev.off()
-}
 
 ##' @template app-template
 ##' @templateVar method ezMethodPbsv(input=NA, output=NA, param=NA)
