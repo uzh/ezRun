9c113a6730bb9a61bb69185a06abe7a3c7e4c2af
Weihong Qi <weihong.qi@fgcz.ethz.ch>
2023-05-15 11:34:01 +0200

    app-Gce file name

--- R/app-Gce.R
+++ R/app-Gce.R
@@ -14,9 +14,9 @@ ezMethodGce = function(input=NA, output=NA, param=NA, htmlFile="00index.html"){
   ezSystem(cmd)
   cmd = paste("/usr/local/ngseq/src/kmerfreq/kmerfreq -k", param$kSize, "-t", ezThreads(), "-f 1 -w 1 -c 1 -q 1 -p", sampleName, paste0(sampleName, ".lib"))
   ezSystem(cmd)
-  cmd = paste("perl /usr/local/ngseq/src/GCE/gce-alternative/estimate_genome_character_real.pl", paste0(sampleName, "kmer.freq.stat"))
+  cmd = paste("perl /usr/local/ngseq/src/GCE/gce-alternative/estimate_genome_character_real.pl", paste0(sampleName, ".kmer.freq.stat"))
   ezSystem(cmd)
-  cmd = paste("convert", paste0(sampleName, "kmer.freq.stat_real_theory_distribution.svg"), paste0(sampleName, "kmer.freq.stat_real_theory_distribution.png"))
+  cmd = paste("convert", paste0(sampleName, "kmer.freq.stat_real_theory_distribution.svg"), paste0(sampleName, ".kmer.freq.stat_real_theory_distribution.png"))
   ezSystem(cmd)
 
   #html report)
