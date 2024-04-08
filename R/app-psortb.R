7a6163866d06f894371707da6e8df5edf63a852b
Natalia Zajac <natalia.zajac@fgcz.ethz.ch>
2024-04-08 12:39:53 +0200

    proteins file not copied

--- R/app-psortb.R
+++ R/app-psortb.R
@@ -18,9 +18,9 @@ ezMethodPsortb = function(input=NA, output=NA, param=NA, htmlFile="00index.html"
   outfile <- list.files(paste0(wddir, "/", sampleName), pattern="_psortb_.*\\.txt")
   outfile <- file.path(wddir, sampleName, outfile)
   ezSystem(paste("cp", outfile, basename(output$getColumn("PsortbOut"))))
-  proteins <- list.files(paste0(wddir, "/", sampleName), pattern=".proteins")
-  proteins <- file.path(wddir, sampleName, proteins)
-  ezSystem(paste("cp", proteins, basename(output$getColumn("Proteins"))))
+  # proteins <- list.files(paste0(wddir, "/", sampleName), pattern=".proteins")
+  # proteins <- file.path(wddir, sampleName, proteins)
+  # ezSystem(paste("cp", proteins, basename(output$getColumn("Proteins"))))
   return("Success")
 }
 
