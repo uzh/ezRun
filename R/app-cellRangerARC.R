1d45772167178c54439ea7d2340e1ace980f661c
Paul Gueguen <pgueguen@fgcz-net.unizh.ch>
2024-05-10 13:06:28 +0200

    Switch to exclude introns

--- R/app-cellRangerARC.R
+++ R/app-cellRangerARC.R
@@ -95,7 +95,7 @@ ezMethodCellRangerARC <- function(input = NA, output = NA, param = NA) {
              paste0("--localmem=", param$ram),
              paste0("--localcores=", param$cores),
              if (ezIsSpecified(param$expectedCells)) {paste0("--expect-cells=", param$expectedCells)},
-             ifelse(ezIsSpecified(param$includeIntrons) && param$includeIntrons, "--gex-exclude-introns=false", "--gex-exclude-introns=true")
+             ifelse(ezIsSpecified(param$excludeIntrons) && param$excludeIntrons, "--gex-exclude-introns=true", "--gex-exclude-introns=false")
            )
                 
   
@@ -354,7 +354,7 @@ EzAppCellRangerARC <-
                       DefaultValue = 10000,
                       Description = "Expected number of cells."
                     ),
-                    includeIntrons = ezFrame(
+                    excludeIntrons = ezFrame(
                       Type = "logical",
                       DefaultValue = TRUE,
                       Description = "Count reads on introns."
