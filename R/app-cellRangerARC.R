5122dd958c54be659540ac5e8bd341cb7763ffcf
Paul Gueguen <pgueguen@fgcz-net.unizh.ch>
2024-05-11 11:28:07 +0200

    Fix --gex-exclude-intron ifelse

--- R/app-cellRangerARC.R
+++ R/app-cellRangerARC.R
@@ -95,7 +95,7 @@ ezMethodCellRangerARC <- function(input = NA, output = NA, param = NA) {
              paste0("--localmem=", param$ram),
              paste0("--localcores=", param$cores),
              if (ezIsSpecified(param$expectedCells)) {paste0("--expect-cells=", param$expectedCells)},
-             ifelse(ezIsSpecified(param$excludeIntrons) && param$excludeIntrons, "--gex-exclude-introns")
+             ifelse(ezIsSpecified(param$excludeIntrons) && param$excludeIntrons, "--gex-exclude-introns", "")
            )
                 
   
