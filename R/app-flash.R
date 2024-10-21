7ac8bed9bea50a227a21f29206abcbed5f3c3526
Lennart Opitz <lopitz@fgcz.ethz.ch>
2024-10-21 14:05:37 +0200

    fix flash

--- R/app-flash.R
+++ R/app-flash.R
@@ -16,7 +16,9 @@ ezMethodFlash = function(input=NA, output=NA, param=NA){
     cmd = paste("flash",trimmedInput$getColumn("Read1"), trimmedInput$getColumn("Read2"),
               "-o",sampleName,'-t',ezThreads(),opt,"1>> ",paste0(sampleName,"_preprocessing.log"))
     ezSystem(cmd)
-    cmd = paste0('pigz --best ',sampleName,'.R1.fastq')
+    cmd = paste0('pigz --best ',sampleName,'.extendedFrags.fastq')
+    ezSystem(cmd)
+    cmd = paste('mv', paste0(sampleName,'.extendedFrags.fastq.gz'), paste0(sampleName,'.R1.fastq.gz'))
     ezSystem(cmd)
   } else {
       ezSystem(paste('mv', paste0(sampleName,'-trimmed_R1.fastq.gz'), paste0(sampleName,'.R1.fastq.gz')))
