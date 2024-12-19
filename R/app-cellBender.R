40342bc0ed144f60c27c412065ab864b682fb1b2
Paul Gueguen <paul.gueguen@fgcz.ethz.ch>
2024-12-19 16:04:04 +0100

    Make the app compatible with cellranger multi

--- R/app-cellBender.R
+++ R/app-cellBender.R
@@ -8,51 +8,56 @@
 EzAppCellBender <-
     setRefClass("EzAppCellBender",contains = "EzApp",
                 methods = list(
-                    initialize = function() {
-                        "Initializes the application using its specific defaults."
-                        runMethod <<- ezMethodCellBender
-                        name <<- "EzAppCellBender"
-                        appDefaults <<- rbind(
-                            cmdOptions=ezFrame(Type="character", DefaultValue="", Description="for -expected-cells and --total-droplets-included"),
-                            gpuMode = ezFrame(
-                                Type = "logical",
-                                DefaultValue = FALSE,
-                                Description = "run with cuda option"
-                                )
-                        )
-                    }
-                        )
+                  initialize = function() {
+                    "Initializes the application using its specific defaults."
+                    runMethod <<- ezMethodCellBender
+                    name <<- "EzAppCellBender"
+                    appDefaults <<- rbind(
+                      cmdOptions=ezFrame(Type="character", DefaultValue="", Description="for -expected-cells and --total-droplets-included"),
+                      gpuMode = ezFrame(
+                        Type = "logical",
+                        DefaultValue = FALSE,
+                        Description = "run with cuda option"
+                      )
+                    )
+                  }
                 )
+    )
 
 ezMethodCellBender <- function(input = NA, output = NA, param = NA) {
-    sampleName = input$getNames()
-    setwdNew(sampleName)
-    cmDir <- input$getFullPaths("UnfilteredCountMatrix")
+  sampleName = input$getNames()
+  setwdNew(sampleName)
+  cmDir <- input$getFullPaths("UnfilteredCountMatrix")
+  inputFile <- paste0(cmDir,'.h5')
+  if(!file.exists(inputFile)){
+    # Try CellRanger multi path if single sample not found
+    cmDir <- file.path(input$getFullPaths("ResultDir"), "multi/count/raw_feature_bc_matrix")
     inputFile <- paste0(cmDir,'.h5')
     if(!file.exists(inputFile)){
-        stop('RawCountMatrix missing! Unsupported InputFormat.')
-    } else
-    cmd <- paste("cellbender remove-background",
-    "--input", inputFile,
-    "--output cellbender.h5")
-    
-    if(param$cmdOptions!=''){
-        cmd <- paste(cmd, cmdOptions)
+      stop('RawCountMatrix missing! Unsupported InputFormat.')
     }
-    
-    if(param$gpuMode){
-        cmd <- paste(cmd, "--cuda")
-    } else {
-        cmd <- paste(cmd, '--cpu-threads', param$cores)
-    }
-    system(cmd)
-    
-    ##Post processing for Seurat
-    cmd <- paste("ptrepack --complevel 5 cellbender_filtered.h5:/matrix cellbender_filtered_seurat.h5:/matrix")
-    system(cmd)
-    cmd <- paste("ptrepack --complevel 5 cellbender.h5:/matrix cellbender_raw_seurat.h5:/matrix")
-    system(cmd)
-    ##Clean Up:
-    system('rm ckpt.tar.gz cellbender_posterior.h5 cellbender.h5 cellbender_filtered.h5')
-    return("Success")
+  }
+  cmd <- paste("cellbender remove-background",
+               "--input", inputFile,
+               "--output cellbender.h5")
+  
+  if(param$cmdOptions!=''){
+    cmd <- paste(cmd, cmdOptions)
+  }
+  
+  if(param$gpuMode){
+    cmd <- paste(cmd, "--cuda")
+  } else {
+    cmd <- paste(cmd, '--cpu-threads', param$cores)
+  }
+  system(cmd)
+  
+  ##Post processing for Seurat
+  cmd <- paste("ptrepack --complevel 5 cellbender_filtered.h5:/matrix cellbender_filtered_seurat.h5:/matrix")
+  system(cmd)
+  cmd <- paste("ptrepack --complevel 5 cellbender.h5:/matrix cellbender_raw_seurat.h5:/matrix")
+  system(cmd)
+  ##Clean Up:
+  system('rm ckpt.tar.gz cellbender_posterior.h5 cellbender.h5 cellbender_filtered.h5')
+  return("Success")
 }
