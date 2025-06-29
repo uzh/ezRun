b30342457ee3a132cca958372d11c6d63fe47283
Lennart Opitz <lopitz@fgcz.ethz.ch>
2025-06-29 11:30:00 +0200

    fix outDir path

--- R/app-NfCoreAtacSeq.R
+++ R/app-NfCoreAtacSeq.R
@@ -131,7 +131,7 @@ getData <- function(output, param){
   dds$Condition <- dsgn$Condition
   design(dds) <- ~ Condition
   
-  outDir <- paste0(basename(output$getColumn('Result')), '/', param$name, '_results/diffpeak_analysis')
+  outDir <- file.path(basename(output$getColumn('Result')),'diffpeak_analysis')
   if(!dir.exists(outDir)) dir.create(outDir, recursive = TRUE)
   setwdNew(outDir)
   
