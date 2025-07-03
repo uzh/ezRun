9791e1258ee3f1a932841c52b2ceb32be1541cfd
Weihong Qi <weihong.qi@fgcz.ethz.ch>
2025-07-03 10:45:40 +0200

    remove inputType

--- R/app-canu.R
+++ R/app-canu.R
@@ -11,20 +11,19 @@ ezMethodCanu = function(input=NA, output=NA, param=NA, htmlFile="00index.html"){
   sampleName = input$getNames()
   SMRT_Path = input$getFullPaths("Read1")
   SMRT_File = basename(input$getColumn("Read1"))
-  inputFileType = param$inputType
-  if (inputFileType == "pacbioSmrtCell"){
-  ezSystem(paste("cp -r", SMRT_Path, "."))
-  ezSystem(paste("mkdir", "smrt_input"))
-  ezSystem(paste("tar -zxf", SMRT_File, "--strip-components=4 -C smrt_input"))
-  readFile = file.path(getwd(), "smrt_input", "Analysis_Results", "*.subreads.fastq") 
-  ezSystem(paste("cat", readFile, ">", paste0(sampleName,".fastq")))
-  cmd = paste("canu", "-p", "canu", "-d", "canu", paste0("genomeSize=", param$canuGenomeSize, "k"), paste0("maxMemory=", param$ram), paste0("maxThreads=", ezThreads()), opt, 
-                param$canuReadOpt, paste0(sampleName,".fastq"), "1> ", paste0(sampleName,"_canu.log"))
-  ezSystem(cmd)
-  ezSystem(paste("cp", "canu/canu.contigs.fasta", basename(output$getColumn("Draft"))))
-  ezSystem(paste("mv", "canu", sampleName))
-  return("Success")
-  }else{
+  #inputFileType = param$inputType
+  #if (inputFileType == "pacbioSmrtCell"){
+  #ezSystem(paste("cp -r", SMRT_Path, "."))
+  #ezSystem(paste("mkdir", "smrt_input"))
+  #ezSystem(paste("tar -zxf", SMRT_File, "--strip-components=4 -C smrt_input"))
+  #readFile = file.path(getwd(), "smrt_input", "Analysis_Results", "*.subreads.fastq") 
+  #ezSystem(paste("cat", readFile, ">", paste0(sampleName,".fastq")))
+  #cmd = paste("canu", "-p", "canu", "-d", "canu", paste0("genomeSize=", param$canuGenomeSize, "k"), paste0("maxMemory=", param$ram), paste0("maxThreads=", ezThreads()), opt, param$canuReadOpt, paste0(sampleName,".fastq"), "1> ", paste0(sampleName,"_canu.log"))
+  #ezSystem(cmd)
+  #ezSystem(paste("cp", "canu/canu.contigs.fasta", basename(output$getColumn("Draft"))))
+  #ezSystem(paste("mv", "canu", sampleName))
+  #return("Success")
+  #}else{
     cmd = paste("canu", "-p", sampleName, "-d", sampleName, 
                 paste0("genomeSize=", param$canuGenomeSize, "k"), 
                 paste0("maxMemory=", param$ram), 
@@ -36,7 +35,7 @@ ezMethodCanu = function(input=NA, output=NA, param=NA, htmlFile="00index.html"){
     ezSystem(paste("cp", contigOutputFile, 
                    basename(output$getColumn("Draft"))))
     return("Success")
-  }
+  #}
 }
 
 ##' @template app-template
