3d820fb1e7205751ddaa66091f136acdf9b3b6eb
Lennart Opitz <lopitz@fgcz.ethz.ch>
2025-10-17 11:42:59 +0200

    set cache dir for nfcore and add annotation track for IGV

--- R/util.R
+++ R/util.R
@@ -970,3 +970,7 @@ ezLoadRobj <- function(filePath, nthreads=1L) {
     }
   )
 }
+
+setNFCacheDir <- function(cacheDir = '/misc/fgcz01/nextflow_apptainer_cache'){
+    ezSetEnv('NXF_APPTAINER_CACHEDIR', cacheDir)
+}
