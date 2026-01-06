ffc5de937d7822f619c6f950d8a100809210ca6c
hrehrauer <Hubert.Rehrauer@fgcz.uzh.ch>
2026-01-06 09:44:58 +0100

    fix bam sort

--- R/app-mapping.R
+++ R/app-mapping.R
@@ -391,10 +391,10 @@ ezMethodSTAR <- function(input = NA, output = NA, param = NA) {
   
   if(ezIsSpecified(param$barcodePattern) && param$barcodePattern!=''){ #Deduplicated based on UMI
       deDupBamFile <- sub('.bam', '_dedup.bam', basename(bamFile))
-      cmd <- paste0('umi_tools dedup --paired --no-sort-output --stdin=',basename(bamFile),' --stdout=', deDupBamFile,' --log=',basename(bamFile),'.log', ' --output-stats=',basename(bamFile),'.stats')
+      cmd <- paste0('umi_tools dedup --temp-dir=. --verbose=0 --paired --no-sort-output --stdin=',basename(bamFile),' --stdout=', deDupBamFile,' --log=',basename(bamFile),'.log', ' --output-stats=',basename(bamFile),'.stats')
       ezSystem(cmd)
-      ezSystem(paste('mv', deDupBamFile, basename(bamFile)))
-      ezSortIndexBam(basename(bamFile), ram=sortRam, cores = nSortThreads)
+      ezSortIndexBam(deDubBamFile, basename(bamFile), ram=sortRam, cores = nSortThreads)
+      file.remove(deDupBamFile)
       tryCatch({local_CondaEnv("gi_py3.11.5", pathToMiniConda = "/usr/local/ngseq/miniforge3")}, warning = function(warning_condition) {cat('warning')}, 
                error = function(error_condition) {cat('error')})
   }
