5fafd82ba15ae0bd942240e68474c3d095acac7e
hubert <Hubert.Rehrauer@fgcz.uzh.ch>
2026-02-06 09:15:40 +0100

    add installation

--- R/app-DIANN.R
+++ R/app-DIANN.R
@@ -14,7 +14,8 @@ ezMethodDIANN = function(input=NA, output=NA, param=NA,
   dir.create("work")
   ## do not need ezRef here; and can't write it to yaml anyway --> remove it
   param$ezRef <- NULL
-  write_yaml(list(param=param), "work/params.yml")
+  write_yaml(list(param=param,
+                  registration=list(workunit_id="0000", container_id="1111")), "work/params.yml")
   ## get the input
   rawDir <- "work/input/raw"
   dir.create(rawDir, recursive = TRUE)
@@ -23,7 +24,6 @@ ezMethodDIANN = function(input=NA, output=NA, param=NA,
   for (x in rawUrl){
     ezSystem(paste("scp", x, rawDir))
   }
-  
   
   cmd <- paste0("scp ", "fgcz-r-033:", param$`03b_additional_fasta_database_path`,
   " work/input/", basename(param$`03b_additional_fasta_database_path`))
@@ -37,17 +37,46 @@ ezMethodDIANN = function(input=NA, output=NA, param=NA,
     rename("Relative Path"=RAW) |>
     mutate(File=basename(`Relative Path`))
   write_csv(ds[ , c("Resource", "Relative Path", "Name", "Grouping Var", "File")], "work/dataset.csv")
+
   
-  
-  snakeFile <- system.file("templates/Snakefile.DIANN3step.smk", package = "ezRun", mustWork = TRUE)
-  file.copy(snakeFile, basename(snakeFile))
+  ## get the DIANN-RUNNER:  
+  ezSystem("git clone https://github.com/wolski/diann-runner.git")
+  snakeFile <- "diann-runner/src/diann_runner/Snakefile.DIANN3step.smk"
+  stopifnot(file.exists(snakeFile))
+  #snakeFile <- system.file("templates/Snakefile.DIANN3step.smk", package = "ezRun", mustWork = TRUE)
+  #file.copy(snakeFile, basename(snakeFile))
   ## example cmd: snakemake uses the on-the-fly generated environment
   ## snakemake -s /scratch/cache/bfabric/bfabric_app_runner/ephemeral/env_dbox3jko/lib/python3.13/site-packages/diann_runner/Snakefile.DIANN3step.smk --cores 64 -p all -d /scratch/5858/A386_DIANN_v23/WU339908/work
-  cmd <- paste("snakemake",
-               "-s", basename(snakeFile),
-               "--cores 64 -p all",
-               "-d ./work")
-  ezSystem(cmd)
+  snakeCmd <- paste("snakemake",
+                    "-s", snakeFile,
+                    "--cores 64 -p all",
+                    "-d ./work")
+  fullCmd <- paste(
+    "uv venv",
+    "source .venv/bin/activate",
+    "uv pip install -e diann-runner", 
+    snakeCmd,
+    collapse = "; ")
+  ezSystem(fullCmd)
+  
+  # 
+  # 
+  # ## not needed: python -m diann_runner.snakemake_cli --cores 64 -p all -d work
+  # cp ./diann-runner/src/diann_runner/Snakefile.DIANN3step.smk Snakefile.DIANN3step.smk
+  # snakemake -s Snakefile.DIANN3step.smk --cores 64 -p all -d work
+  # 
+  # setUpCmd <- paste(
+  # "git clone https://github.com/wolski/diann-runner.git",
+  # "uv venv",
+  # "source .venv/bin/activate",
+  # "uv pip install -e diann-runner", 
+  # snakeCmd
+  # collapse = "; ")
+  # 
+  #   ezSystem(cmd)
+  
+  ##
+
   return("Success")
 }
 
